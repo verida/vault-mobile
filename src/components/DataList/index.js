be6cd34b88f5188e6c95260db0d70be23e82cfa3
rodion-malakhdev <rodion.malakhdev@gmail.com>
2020-09-22 12:37:23 +0300

    feat: data page

---
+++ src/components/DataList/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import DataListItem from './DataListItem';
+
+export default ({ list }) => list.map((item, index) => {
+    return <DataListItem key={`data-list-${index}`} item={item} />;
+});
--- src/pages/Dashboard/Data.js
+++ src/pages/Dashboard/Data.js
@@ -1,48 +1,52 @@
 import React from 'react';
-import { StyleSheet, Linking, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Actions } from 'react-native-router-flux';
+import { Container, Content, List } from 'native-base';
 
-import Text from '../../components/Text';
-import { BLACK_COLOR_OPACITY, PRIMARY_COLOR } from '../../constants/color';
-import { NUNITO_SANS_BOLD, NUNITO_SANS_SEMIBOLD } from '../../constants/text';
+import DataList from '../../components/DataList';
 
-const vaultURL = 'http://vault.verida.io/';
-const openVault = () => Linking.openURL(vaultURL);
+import IdentitySvg from '../../assets/icons/data/identity.svg';
+import HealthSvg from '../../assets/icons/data/health.svg';
+import EmploymentSvg from '../../assets/icons/data/employment.svg';
+import FinanceSvg from '../../assets/icons/data/finance.svg';
+import QualificationsSvg from '../../assets/icons/data/qualifications.svg';
+import InsuranceSvg from '../../assets/icons/data/insurance.svg';
+import SocialSvg from '../../assets/icons/data/social.svg';
+import SubscriptionsSvg from '../../assets/icons/data/subscriptions.svg';
+import TicketsSvg from '../../assets/icons/data/tickets.svg';
+import DocumentsSvg from '../../assets/icons/data/documents.svg';
+
+import {
+    IDENTITY,
+    HEALTH,
+    EMPLOYMENT,
+    FINANCE,
+    QUALIFICATIONS,
+    INSURANCE,
+    SOCIAL,
+    SUBSCRIPTIONS,
+    TICKETS,
+    DOCUMENTS,
+} from '../../constants/route';
 
 export default () => (
-    <View style={style.container}>
-        <Text style={style.title}>View Data</Text>
-        <Text style={style.text}>You can view all your encrypted data by logging into the Verida Vault</Text>
-        <TouchableOpacity onPress={openVault}>
-            <Text style={style.link}>{vaultURL}</Text>
-        </TouchableOpacity>
-    </View>
+    <Container>
+        <Content>
+            <List>
+                <DataList list={list} />
+            </List>
+        </Content>
+    </Container>
 );
 
-const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        paddingHorizontal: 57,
-        paddingVertical: 40
-    },
-    title: {
-        fontFamily: NUNITO_SANS_BOLD,
-        fontSize: 22
-    },
-    text: {
-        textAlign: 'center',
-        fontFamily: NUNITO_SANS_BOLD,
-        color: BLACK_COLOR_OPACITY(0.6),
-        fontSize: 14,
-        lineHeight: 19,
-        marginVertical: 7
-    },
-    link: {
-        color: PRIMARY_COLOR,
-        textDecorationLine: 'underline',
-        fontFamily: NUNITO_SANS_SEMIBOLD,
-        fontSize: 16,
-        lineHeight: 22
-    }
-});
+const list = [
+    { label: 'Identity', icon: <IdentitySvg />, onPress: () => Actions[IDENTITY]() },
+    { label: 'Health', icon: <HealthSvg />, onPress: () => Actions[HEALTH]() },
+    { label: 'Employment', icon: <EmploymentSvg />, onPress: () => Actions[EMPLOYMENT]() },
+    { label: 'Finance', icon: <FinanceSvg />, onPress: () => Actions[FINANCE]() },
+    { label: 'Qualifications', icon: <QualificationsSvg />, onPress: () => Actions[QUALIFICATIONS]() },
+    { label: 'Insurance', icon: <InsuranceSvg />, onPress: () => Actions[INSURANCE]() },
+    { label: 'Social', icon: <SocialSvg />, onPress: () => Actions[SOCIAL]() },
+    { label: 'Subscriptions', icon: <SubscriptionsSvg />, onPress: () => Actions[SUBSCRIPTIONS]() },
+    { label: 'Tickets', icon: <TicketsSvg />, onPress: () => Actions[TICKETS]() },
+    { label: 'Documents', icon: <DocumentsSvg />, onPress: () => Actions[DOCUMENTS]() },
+];
