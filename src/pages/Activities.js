6d169c1b364f362ff503280447d78c0f7f9de6dd
rodion-malakhdev <rodion.malakhdev@gmail.com>
2020-09-22 13:27:51 +0300

    feat(health page): activities page

---
+++ src/pages/Activities.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Content, Container } from 'native-base';
+
+import ActivitiesList from '../components/ActivitiesList';
+import CustomHeader from '../components/CustomHeader';
+
+import StravaSvg from '../assets/icons/strava.svg';
+
+export default () => (
+    <View>
+        <CustomHeader title="Activities" rightButton={{ icon: 'ios-add-circle' }} />
+        <View style={style.itemsList}>
+            <Container>
+                <Content>
+                    <ActivitiesList list={list} />
+                </Content>
+            </Container>
+        </View>
+    </View>
+);
+
+const list = [
+    { label: 'Afternoon Run', icon: <StravaSvg />, date: '11/10/2018', distance: { value: 6.42, unitOfMeasure: 'ml' }, calories: 737 },
+    { label: 'Cycling', icon: <StravaSvg />, date: '09/10/2018', distance: { value: 281.4, unitOfMeasure: 'km' }, calories: 1203 },
+];
+
+const style = StyleSheet.create({
+    itemsList: {
+        flex: 1,
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+});
