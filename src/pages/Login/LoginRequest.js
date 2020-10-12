fd2b3a672bbf7f140aa3b3c3d44b27ca97a11af7
rodion-malakhdev <rodion.malakhdev@gmail.com>
2020-10-12 14:04:43 +0300

    refactor: user login screens

--- src/pages/Login/LoginRequest.js
+++ src/pages/Login/LoginRequest.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, StyleSheet, Linking } from 'react-native';
+import { Icon, Button as NButton } from 'native-base';
 
-import VaultLogoInv from '../../assets/vault-logo-inv.svg';
+import StravaLogo from '../../assets/strava-logo.svg';
 import MobileSvg from '../../assets/mobile.svg';
 
 import Text from '../../components/Text';
@@ -10,6 +11,7 @@ import { Actions } from 'react-native-router-flux';
 import { LOGIN_HISTORY } from '../../constants/route';
 
 import { NUNITO_SANS_BOLD, NUNITO_SANS_SEMIBOLD } from '../../constants/text';
+import { BLACK_COLOR_OPACITY } from '../../constants/color';
 
 const approve = () => {
     Actions[LOGIN_HISTORY]();
@@ -19,34 +21,76 @@ const deny = () => {
     Actions[LOGIN_HISTORY]();
 };
 
-export default () => (
-    <View style={style.container}>
-        <VaultLogoInv />
-        <MobileSvg style={style.img} />
-        <Text style={style.title}>You have a new request</Text>
-        <Text style={style.text}>
-            There is a new login approval request from http://vault.verida.io/
-        </Text>
-        <Text style={style.text}>
-            25 May, 2020 at 2:53 pm
-        </Text>
+export default (props) => {
+    const [isModalVisible, setModalVisibility] = useState(!props.verified);
 
-        <View style={style.actions}>
-            <Button style={[style.btn, style.mr]} onPress={approve}>Approve</Button>
-            <Button style={style.btn} color="grey" onPress={deny}>Deny</Button>
-        </View>
-    </View>
-);
+    const color = props.verified ? '#37D5C7' : '#EF7936';
+    const iconName = props.verified ? 'check' : 'exclamationcircleo';
+
+    return (
+        <ScrollView contentContainerStyle={style.container}>
+            <View style={{ alignItems: 'center' }}>
+                <StravaLogo />
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={[style.text, { fontSize: 12, color }]}>
+                        <Icon type="AntDesign" name={iconName} style={[style.text, { color }]} />
+                        {props.verified ? null : ' Not'}
+                        {' Verified'}
+                    </Text>
+                </View>
+                <MobileSvg style={style.img} />
+                <Text style={style.title}>New Login Request</Text>
+                <View>
+                    <Text style={style.text}>
+                        There is a new login approval request from
+                    </Text>
+                    <Text style={[style.text, style.link]}
+                        onPress={() => Linking.openURL('http://www.strava.com/')}>
+                    http://www.strava.com/
+                    </Text>
+                </View>
+                <Text style={style.text}>
+                    25 May, 2020 at 2:53 pm
+                </Text>
+                <Text style={[style.text, style.timeout]}>
+                    Expires in 90 seconds
+                </Text>
+            </View>
+
+            {
+                isModalVisible
+                    ? (<View style={style.modal}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text style={[style.text, { color }]}>
+                                <Icon type='AntDesign' name='exclamationcircleo' style={[style.text, { color }]} />
+                                {' Security Warning'}
+                            </Text>
+                            <NButton transparent style={{ position: 'absolute', right: 0 }} onPress={() => setModalVisibility(!isModalVisible)}>
+                                <Icon type='AntDesign' name='close' style={{ color: '#000', fontSize: 17 }} />
+                            </NButton>
+                        </View>
+                        <Text style={[style.text, { textAlign: 'left', fontSize: 12 }]}>Website could not be verified and is untrusted.</Text>
+                    </View>)
+                    : null
+            }
+
+            <View style={style.actions}>
+                <Button style={[style.btn, style.mr]} onPress={approve}>Login</Button>
+                <Button style={style.btn} color="grey" onPress={deny}>Ignore</Button>
+            </View>
+        </ScrollView>
+    );
+};
 
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        justifyContent: 'space-between',
         marginVertical: 55,
         marginHorizontal: 20
     },
     img: {
-        marginTop: 51,
+        marginTop: 35,
         marginBottom: 20
     },
     title: {
@@ -61,9 +105,15 @@ const style = StyleSheet.create({
         textAlign: 'center',
         marginVertical: 8
     },
+    timeout: {
+        fontSize: 12,
+        color: BLACK_COLOR_OPACITY(0.6)
+    },
+    link: {
+        color: 'blue'
+    },
     actions: {
-        position: 'absolute',
-        bottom: 0,
+        marginTop: 30,
         flexDirection: 'row',
     },
     btn: {
@@ -72,5 +122,12 @@ const style = StyleSheet.create({
     },
     mr: {
         marginRight: 20
+    },
+    modal: {
+        backgroundColor: '#FDF4EA',
+        paddingLeft: 15,
+        marginTop: 10,
+        width: '100%',
+        borderRadius: 5
     }
 });
