deea7b7b2fe1029a85827ae0e83b756f0e9497f0
rodion-malakhdev <67914649+rodion-malakhdev@users.noreply.github.com>
2020-10-19 08:44:44 +0300

    refactor: user login screens to match updated designs (#11)

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Dimensions, TouchableOpacity } from 'react-native';
 import Text from '../../components/Text';
 import { Actions } from 'react-native-router-flux';
 import { LOGIN_REQUEST } from '../../constants/route';
 import { BLACK_COLOR_OPACITY, LIGHTGREY_COLOR, WHITE_COLOR } from '../../constants/color';
 import { NUNITO_SANS_BOLD, NUNITO_SANS_SEMIBOLD } from '../../constants/text';
 
-const onPress = () => Actions[LOGIN_REQUEST]();
+const onPress = (props) => Actions[LOGIN_REQUEST](props);
 
 export default ({ data }) => {
     return (
-        <TouchableOpacity style={style.card} onPress={onPress}>
+        <TouchableOpacity style={style.card} onPress={() => onPress({ verified: false })}>
             <View style={style.details}>
-                <Image source={data.img} width={40} height={40} />
+                {data.img}
                 <View style={style.description}>
                     <Text style={style.title}>
                         {data.title}
@@ -21,7 +21,9 @@ export default ({ data }) => {
                         {data.expired}
                     </Text>
                 </View>
-                <Text style={[style.text, style.time]}>{data.time}</Text>
+                <View>
+                    <Text style={[style.text, style.time]}>{data.time}</Text>
+                </View>
             </View>
         </TouchableOpacity>
     );
@@ -54,6 +56,7 @@ const style = StyleSheet.create({
         fontFamily: NUNITO_SANS_SEMIBOLD
     },
     time: {
+        textAlign: 'right',
         color: BLACK_COLOR_OPACITY(0.6)
     }
 });
