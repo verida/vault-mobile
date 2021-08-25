3906534014e221cb9d8cbb7449f4ddc6afc7a364
Hien Phung <pkhien95@gmail.com>
2021-08-25 15:12:49 +0700

    #64: Improve deeplink

--- src/navigation/linkingConfiguration.ts
+++ src/navigation/linkingConfiguration.ts
@@ -2,7 +2,7 @@ import { LinkingOptions } from '@react-navigation/native'
 import { RootStackParams } from 'navigation/types'
 
 const linking: LinkingOptions<RootStackParams> = {
-  prefixes: ['veridavault://'],
+  prefixes: ['veridavault://', 'https://vault.verida.io/sso'],
   config: {
     screens: {
       Main: {
