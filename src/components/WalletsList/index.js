e90b103652ac42d59a0756852ee663a64ef33b05
saadibrahim <saad.lakhany@gmail.com>
2021-10-13 19:41:56 +0400

    add swipe menu support to wallet list

--- src/components/WalletsList/index.js
+++ src/components/WalletsList/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { SwipeListView } from 'react-native-swipe-list-view'
+import { Text, View } from 'react-native'
 
 import WalletsListItem from './WalletsListItem'
 
-export default ({ list }) =>
-  list.map((item, index) => {
-    return <WalletsListItem key={`wallets-list-${index}`} item={item} />
-  })
+export default ({ list }) => {
+  return (
+    <SwipeListView
+      data={list}
+      renderItem={(data) => (
+        <View style={{ marginTop: data.item.other ? 30 : 0 }}>
+          <WalletsListItem item={data.item} />
+        </View>
+      )}
+      renderHiddenItem={(data) => (
+        <View
+          style={{
+            width: 80,
+            backgroundColor: 'red',
+            alignSelf: 'flex-end',
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginTop: data.item.other ? 30 : 0,
+          }}>
+          <Text style={{ color: '#fff', textAlign: 'center' }}>Remove</Text>
+        </View>
+      )}
+      rightOpenValue={-80}
+    />
+  )
+}
