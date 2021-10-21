7f42abc3d6c1781b0c785a0a4c4f90a310f6234c
saadibrahim <saad.lakhany@gmail.com>
2021-10-21 13:24:44 +0400

    add search bar

--- src/pages/Tokens/SendListModal.js
+++ src/pages/Tokens/SendListModal.js
@@ -9,7 +9,6 @@ import {
 import { Icon, List } from 'native-base'
 
 import NavigationHeader from 'components/Navigation/NavigationHeader'
-import Layout from 'components/Layouts/Layout'
 import TokensList from 'components/Tokens/TokensList'
 
 export default ({ visible, hideModal, list }) => {
@@ -25,6 +24,10 @@ export default ({ visible, hideModal, list }) => {
         }}
         title='Send'
       />
+      <View style={styles.searchContainer}>
+        <Icon name='search-outline' style={styles.searchIcon} />
+        <TextInput style={styles.searchInput} placeholder='Search' />
+      </View>
       <View style={styles.container}>
         <List>
           <TokensList list={list} />
@@ -39,5 +42,22 @@ const styles = StyleSheet.create({
     flex: 1,
     borderTopWidth: 1,
     borderTopColor: 'rgba(4, 17, 51, 0.2)',
+  },
+  searchIcon: { color: 'rgba(60, 60, 67, 0.6)', fontSize: 24 },
+  searchContainer: {
+    backgroundColor: 'rgba(239, 241, 244, 1)',
+    flexDirection: 'row',
+    marginVertical: 10,
+    marginTop: 0,
+    marginHorizontal: 15,
+    alignItems: 'center',
+    paddingHorizontal: 8,
+    paddingVertical: 7,
+    borderRadius: 10,
+  },
+  searchInput: {
+    fontSize: 18,
+    marginLeft: 4,
+    color: 'rgba(60, 60, 67, 0.6)',
   },
 })
