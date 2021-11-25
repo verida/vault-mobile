549617b7a84cf4c75670f6fb1ab86a8b425b49f1
saadibrahim <saad.lakhany@gmail.com>
2021-11-25 20:05:19 +0400

    update constant to remove chain, add CAIP address

--- src/wallet/constants.ts
+++ src/wallet/constants.ts
@@ -3,14 +3,12 @@ export const SUPPORTED_TOKENS = [
     name: 'Ethereum',
     symbol: 'ETH',
     logo: '...',
-    address: null,
-    chain: 'ethereum',
+    address: 'eip155:1/slip44:60',
   },
   {
     name: 'Tether',
     symbol: 'USDT',
     logo: '...',
-    address: '0x2msdda2829lsm',
-    chain: 'ethereum',
+    address: 'eip155:1/erc20:0x6b175474e89094c44da98b954eedeac495271d0f',
   },
 ]
--- src/wallet/types.ts
+++ src/wallet/types.ts
@@ -2,13 +2,6 @@ import { AccountId, AssetType } from 'caip'
 
 export type Address = string
 
-// export type Token = {
-//   name: string
-//   symbol: string
-//   logo: string
-//   address: Address
-// }
-
 export type Transaction = {
   id: string
   from: AccountId.AccountIdParams
