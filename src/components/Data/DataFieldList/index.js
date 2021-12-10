da92f9f27f05f41e448c77406644a9471ee38d94
Hien Phung <pkhien95@gmail.com>
2021-12-10 12:46:02 +0700

    Fix lint

--- src/components/Data/DataFieldList/index.js
+++ src/components/Data/DataFieldList/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import DataFieldItem from './DataFieldItem'
 
-export default ({ data, grid }) =>
+export default ({ data }) =>
   data.data.map((item, index) => {
     return <DataFieldItem key={`data-field-${index}`} item={item} data={data} />
   })
