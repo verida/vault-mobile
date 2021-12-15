6b9929a5cad97c03d9e4a89b938391950b061a5d
Hien Phung <pkhien95@gmail.com>
2021-12-15 16:20:50 +0700

    Fix lint

--- src/components/Navigation/LoginTabs.js
+++ src/components/Navigation/LoginTabs.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import { View, Dimensions, StyleSheet, TouchableOpacity } from 'react-native'
+import {
+  Animated,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 
 import { BLACK_COLOR, WHITE_COLOR } from '../../constants/color'
 import { NUNITO_SANS_SEMIBOLD } from '../../constants/text'
