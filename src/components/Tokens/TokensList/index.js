83460a2c8b26d52f3fc31baa3eeed1610bb2b264
Saad Ibrahim <saad.lakhany@gmail.com>
2021-12-15 18:55:09 +0400

    update token list to use flatlist

--- src/components/Tokens/TokensList/index.js
+++ src/components/Tokens/TokensList/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { FlatList } from 'react-native'
 
 import TokensListItem from './TokensListItem'
 
-export default ({ list, onPressItem }) =>
-  list.map((item, index) => {
-    return (
-      <TokensListItem
-        key={`data-list-${index}`}
-        item={item}
-        onPressItem={onPressItem}
-      />
-    )
-  })
+export default ({ list, onPressItem }) => {
+  const renderItem = ({ item }) => (
+    <TokensListItem item={item} onPressItem={onPressItem} />
+  )
+
+  return (
+    <FlatList
+      data={list}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.address}
+    />
+  )
+}
