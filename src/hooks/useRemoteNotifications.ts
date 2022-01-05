925051203623a8600d0bb43f6c53e762eba5eb04
Hien Phung <pkhien95@gmail.com>
2022-01-05 12:31:42 +0700

    Add alert for logs

--- src/hooks/useRemoteNotifications.ts
+++ src/hooks/useRemoteNotifications.ts
@@ -1,10 +1,11 @@
 import messaging from '@react-native-firebase/messaging'
 import { useEffect } from 'react'
+import { Alert } from 'react-native'
 
 export function useRemoteNotifications() {
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      console.log('remoteMessage:', remoteMessage)
+      Alert.alert('Remote notification', JSON.stringify(remoteMessage))
     })
 
     return unsubscribe
