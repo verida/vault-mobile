f2ef971ebfbe4718039e8f4061113572e739ca26
Saad Ibrahim <saad.lakhany@gmail.com>
2022-01-06 22:36:38 +0400

    Update Dashboard.js

--- src/pages/Tokens/Dashboard.js
+++ src/pages/Tokens/Dashboard.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { Container } from 'native-base'
 import { connect } from 'react-redux'
+import { View } from 'react-native'
 
 import NavigationHeader from 'components/Navigation/NavigationHeader'
 import TokenBanner from 'components/Tokens/TokenBanner'
 import TokensList from 'components/Tokens/TokensList'
+import LoadingIndicator from 'components/LoadingIndicator'
 import SendListModal from './SendListModal'
 
 import SettingsSvg from 'assets/icons/settings.svg'
 
-import { getPrices, getBalances, getWallets } from 'reduxStore/wallet/actions'
-import { getListAndTotal } from 'reduxStore/wallet/selectors'
+import { getPrices, getBalances } from 'reduxStore/wallet/actions'
+import { getTokensData } from 'reduxStore/wallet/selectors'
 
-const TokenDashboard = ({
-  navigation,
-  getPrices,
-  getBalances,
-  listAndTotal,
-}) => {
+const TokenDashboard = ({ navigation, getPrices, getBalances, data }) => {
   const [sendModalVisible, setSendModalVisible] = useState(false)
+
+  function pullToRefresh() {
+    getBalances()
+    getPrices()
+  }
+
   useEffect(() => {
     async function loadData() {
       getBalances()
@@ -27,6 +30,8 @@ const TokenDashboard = ({
 
     loadData()
   }, [])
+
+  const { loading, listAndTotal } = data
 
   const { list, total } = listAndTotal
 
@@ -39,34 +44,44 @@ const TokenDashboard = ({
           icon: <SettingsSvg />,
         }}
       />
-      <TokenBanner
-        data={{
-          amount: total,
-        }}
-        // sendButtonAction={() => setSendModalVisible(true)}
-        // buyButtonAction={() => navigation.navigate('BuyToken')}
-        // receiveButtonAction={() => navigation.navigate('ReceiveToken')}
-      />
-      <TokensList
-        list={list}
-        onPressItem={(item) => navigation.navigate('SingleCurrency', { item })}
-      />
-      <SendListModal
-        visible={sendModalVisible}
-        hideModal={() => setSendModalVisible(false)}
-        list={list}
-        onPressItem={() => {
-          setSendModalVisible(false)
-          navigation.navigate('SendToken')
-        }}
-      />
+      {loading ? (
+        <LoadingIndicator />
+      ) : (
+        <View>
+          <TokenBanner
+            data={{
+              amount: total,
+            }}
+            // sendButtonAction={() => setSendModalVisible(true)}
+            // buyButtonAction={() => navigation.navigate('BuyToken')}
+            // receiveButtonAction={() => navigation.navigate('ReceiveToken')}
+          />
+          <TokensList
+            list={list}
+            onPressItem={(item) =>
+              navigation.navigate('SingleCurrency', { item })
+            }
+            onPullToRefresh={() => pullToRefresh()}
+            refreshing={loading}
+          />
+          <SendListModal
+            visible={sendModalVisible}
+            hideModal={() => setSendModalVisible(false)}
+            list={list}
+            onPressItem={() => {
+              setSendModalVisible(false)
+              navigation.navigate('SendToken')
+            }}
+          />
+        </View>
+      )}
     </Container>
   )
 }
 
 const mapStateToProps = (state) => {
   return {
-    listAndTotal: getListAndTotal(state),
+    data: getTokensData(state),
   }
 }
 
