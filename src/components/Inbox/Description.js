5dad023707efb2ceea95c1509b4bdc0cd1a14776
Hien Phung <pkhien95@gmail.com>
2022-01-16 17:34:00 +0700

    Optimize imports

--- src/components/Inbox/Description.js
+++ src/components/Inbox/Description.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import Text from '../Text'
 
-import { BLACK_COLOR_OPACITY } from '../../constants/color'
-import { NUNITO_SANS_BOLD, NUNITO_SANS_SEMIBOLD } from '../../constants/text'
+import { BLACK_COLOR_OPACITY } from 'constants/color'
+import { NUNITO_SANS_BOLD, NUNITO_SANS_SEMIBOLD } from 'constants/text'
 import VeridaSvg from 'assets/icons/verida.svg'
 
 export default ({ details }) => {
