364e1b1a88b28cc6c22594e369e3910ecaf27447
Saad Ibrahim <saad.lakhany@gmail.com>
2022-01-20 14:39:37 +0530

    update single currency page with asset enabling button and warning

--- src/pages/Tokens/SingleCurrency.js
+++ src/pages/Tokens/SingleCurrency.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Container, Icon } from 'native-base'
 import { connect } from 'react-redux'
+import { Alert } from 'react-native'
 import Clipboard from '@react-native-community/clipboard'
 import Toast from 'react-native-root-toast'
 
@@ -8,6 +9,8 @@ import NavigationHeader from 'components/Navigation/NavigationHeader'
 import TokenBanner from 'components/Tokens/TokenBanner'
 import LoadingIndicator from 'components/LoadingIndicator'
 import TransactionsList from 'components/Tokens/TransactionsList'
+import { getTokenChain, isNativeToken } from 'helpers/tokens'
+import { sendTransaction } from 'reduxStore/wallet/actions'
 
 import {
   getTransactionsForToken,
@@ -18,7 +21,10 @@ import {
   selectTransactionsData,
   selectSingleTokenData,
   getWalletsData,
+  selectNativeTokenBalance,
 } from 'reduxStore/wallet/selectors'
+import { TouchableOpacity, Text } from 'react-native'
+import { WARNING_COLOR } from 'constants/color'
 
 const SingleCurrency = ({
   navigation,
@@ -29,6 +35,8 @@ const SingleCurrency = ({
   onGetBalances,
   onGetPrices,
   wallets,
+  onSendTransaction,
+  nativeTokenBalance,
 }) => {
   const { item } = route.params
   const { list, loading } = transactions
@@ -48,6 +56,12 @@ const SingleCurrency = ({
     loadData()
   }, [onGetTransactionsForToken, item])
 
+  const warningRequired =
+    getTokenChain(item.address) === 'algorand' && !isNativeToken(item.address)
+
+  const showAlert = () =>
+    Alert.alert('Not enough balance', 'You need to have at least 0.001 ALGO')
+
   return (
     <Container>
       <NavigationHeader
@@ -57,6 +71,34 @@ const SingleCurrency = ({
         }}
         title={item.label}
       />
+      {warningRequired && loading === false && list.length === 0 && (
+        <TouchableOpacity
+          style={{
+            backgroundColor: WARNING_COLOR,
+            padding: 15,
+            marginHorizontal: 15,
+            borderRadius: 5,
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+          onPress={() => {
+            if (nativeTokenBalance >= 0.001) {
+              onSendTransaction(
+                { token: item, amount: 0, address: address },
+                true
+              )
+            } else {
+              showAlert()
+            }
+          }}>
+          <Icon name='warning' style={{ color: '#fff', marginRight: 10 }} />
+          <Text style={{ color: '#FFF', flex: 1 }}>
+            Algorand network requires a special transaction to enable receiving
+            of non native assets, tap to enable {item.symbol} transactions (Fee:
+            0.001 ALGO).
+          </Text>
+        </TouchableOpacity>
+      )}
       <TokenBanner
         data={tokenData}
         receiveButtonAction={() =>
@@ -100,6 +142,7 @@ const mapStateToProps = (state, props) => {
     ),
     tokenData: selectSingleTokenData(state, props.route.params.item.address),
     wallets: getWalletsData(state),
+    nativeTokenBalance: selectNativeTokenBalance(state),
   }
 }
 
@@ -109,6 +152,8 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(getTransactionsForToken(assetID)),
     onGetPrices: () => dispatch(getPrices()),
     onGetBalances: () => dispatch(getBalances()),
+    onSendTransaction: (params, isAssetEnablingTransaction) =>
+      dispatch(sendTransaction(params, isAssetEnablingTransaction)),
   }
 }
 
