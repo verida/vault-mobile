befe7d7eba03b55cfa55b5ed7565582117570389
Saad Ibrahim <saad.lakhany@gmail.com>
2022-01-31 15:21:10 +0530

    add ethereum tokens

--- src/wallet/constants.js
+++ src/wallet/constants.js
@@ -5,6 +5,7 @@ export const SUPPORTED_TOKENS = [
     icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4030.png',
     address:
       'algorand:wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=/erc20:slip44/1',
+    decimal: 6,
   },
   {
     name: 'USD Coin',
@@ -12,6 +13,7 @@ export const SUPPORTED_TOKENS = [
     icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png',
     address:
       'algorand:wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=/erc20:10458941/1',
+    decimal: 6,
   },
   {
     name: 'PlanetWatch',
@@ -19,6 +21,21 @@ export const SUPPORTED_TOKENS = [
     icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/11861.png',
     address:
       'algorand:wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=/erc20:408947/1',
+    decimal: 6,
+  },
+  {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+    address: 'eip155:4/erc20:slip44/1',
+    decimal: 18,
+  },
+  {
+    name: 'Tether USD',
+    symbol: 'USDT',
+    icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
+    address: 'eip155:4/erc20:0xdac17f958d2ee523a2206206994597c13d831ec7/1',
+    decimal: 18,
   },
 ]
 
