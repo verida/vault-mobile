65de44c0a736cb5976f6f898caac3289c97d60d2
Saad Ibrahim <saad.lakhany@gmail.com>
2022-02-01 14:29:11 +0530

    add decimal helpers

--- src/wallet/helpers/tokens.js
+++ src/wallet/helpers/tokens.js
@@ -13,3 +13,11 @@ export const getTokenChain = (address) => {
   const parsed = AssetId.parse(address)
   return parsed.chainId.namespace
 }
+
+export const handleTokenDecimals = (quantity, decimalPlaces) => {
+  return quantity / Math.pow(10, decimalPlaces)
+}
+
+export const formatTokenQuantity = (quantity, decimalPlaces) => {
+  return handleTokenDecimals(quantity, decimalPlaces).toFixed(3)
+}
