8e7d1a62751d45e8144aad8f9bdb802bec12b21c
Saad Ibrahim <saad.lakhany@gmail.com>
2022-02-23 18:02:55 +0400

    update data consolidation service for send transaction, get transaction params, etc

--- src/wallet/data.js
+++ src/wallet/data.js
@@ -1,31 +1,58 @@
-import { indexerClient } from 'wallet/chains/algorand'
-import { chainsApi } from 'wallet/helpers/api'
+import algosdk from 'algosdk'
+
+import { indexerClient, algodClient } from 'wallet/chains/algorand'
+import { web3 } from 'wallet/chains/ethereum'
+import { moralisApi } from 'wallet/helpers/api'
 import { SUPPORTED_TOKENS } from 'wallet/constants'
 import { getTokenAddress, isNativeToken } from 'wallet/helpers/tokens'
+import {
+  getWalletsData,
+  getTransactionParamsData,
+} from 'reduxStore/wallet/selectors'
+
+const minABI = [
+  // transfer
+  {
+    constant: false,
+    inputs: [
+      {
+        name: '_to',
+        type: 'address',
+      },
+      {
+        name: '_value',
+        type: 'uint256',
+      },
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        name: '',
+        type: 'bool',
+      },
+    ],
+    type: 'function',
+  },
+]
 
 const getAllBalances = async (wallets) => {
   let algorandBalances = await indexerClient
     .lookupAccountByID(wallets.algo.address)
     .do()
 
-  let assets = []
-  SUPPORTED_TOKENS.forEach((token) => {
-    if (token.address.includes('eip155')) {
-      if (token.address.includes('0x0000001')) {
-        assets.push('ethereum/native/eth')
-      } else {
-        assets.push(
-          `ethereum/contract/${getTokenAddress(token.address)}/erc-20`
-        )
-      }
-    }
-  })
-
-  const ethereumBalances = await chainsApi.get(
-    'v2/ethereum/mainnet/account/' +
-      '0x28C6c06298d514Db089934071355E5743bf21d60',
+  const ethNativeBalance = await moralisApi.get(
+    // '0x28C6c06298d514Db089934071355E5743bf21d60' + '/balance',
+    wallets.ethr.address + '/balance',
     {
-      assets: assets.join(','),
+      chain: 'rinkeby',
+    }
+  )
+
+  const ethereumBalances = await moralisApi.get(
+    // '0x28C6c06298d514Db089934071355E5743bf21d60' + '/erc20',
+    wallets.ethr.address + '/erc20',
+    {
+      chain: 'rinkeby',
     }
   )
 
@@ -53,12 +80,15 @@ const getAllBalances = async (wallets) => {
     }
   }
 
+  if (ethNativeBalance.data) {
+    list.ETH = parseFloat(ethNativeBalance.data.balance)
+  }
+
   if (ethereumBalances.data) {
     Object.values(ethereumBalances.data).map((obj) => {
-      let tok
-      tok = SUPPORTED_TOKENS.find((ele) => {
+      let tok = SUPPORTED_TOKENS.find((ele) => {
         // let tokenAddress = getTokenAddress(ele.address)
-        return ele.symbol === obj.currency.symbol
+        return ele.symbol === obj.symbol
       })
       if (tok) {
         list[tok.symbol] = parseFloat(obj.balance)
@@ -71,39 +101,56 @@ const getAllBalances = async (wallets) => {
 
 const getTransactions = async (wallets, tokenAddress) => {
   let transactions = []
-  let assets = []
+  let moralisTransactions
   if (tokenAddress.includes('eip155')) {
-    if (tokenAddress.includes('0x0000001')) {
-      assets.push('ethereum/native/eth')
-    } else {
-      assets.push(`ethereum/contract/${getTokenAddress(tokenAddress)}/erc-20`)
-    }
-    const ethereumTransactions = await chainsApi.get(
-      'v2/ethereum/mainnet/account/' +
-        '0x28C6c06298d514Db089934071355E5743bf21d60/txs',
-      {
-        assets,
-      }
-    )
-    const ethTransactions = ethereumTransactions.data.items
-    // const userAddr = wallets.ethr.address
-    const userAddr = '0x28C6c06298d514Db089934071355E5743bf21d60'
-    if (ethTransactions) {
-      transactions = ethTransactions
-        .filter((tx) => {
-          return !!tx.operations.native
-        })
-        .map((tx) => {
-          let trans = tx.operations.native.detail
-          let isUserSender = trans.from === userAddr
+    const userAddr = wallets.ethr.address
+    // const userAddr = '0x28C6c06298d514Db089934071355E5743bf21d60'
+
+    if (isNativeToken(tokenAddress)) {
+      moralisTransactions = await moralisApi.get(userAddr, {
+        chain: 'rinkeby',
+      })
+
+      const ethTransactions = moralisTransactions.data.result
+      if (ethTransactions) {
+        transactions = ethTransactions.map((tx) => {
+          let isUserSender = tx.from_address === userAddr.toLowerCase()
           return {
-            id: tx.id,
+            id: tx.hash,
             type: isUserSender ? 'sent' : 'received',
-            address: isUserSender ? trans.to : trans.from,
-            quantity: trans.value,
+            address: isUserSender ? tx.to_address : tx.from_address,
+            quantity: tx.value,
             pending: false,
           }
         })
+      }
+    } else {
+      moralisTransactions = await moralisApi.get(
+        userAddr + '/erc20/transfers',
+        {
+          chain: 'rinkeby',
+        }
+      )
+
+      const ethTransactions = moralisTransactions.data.result
+      if (ethTransactions) {
+        let contractAddress = getTokenAddress(tokenAddress)
+
+        transactions = ethTransactions
+          .filter((tx) => {
+            return tx.address === contractAddress.toLowerCase()
+          })
+          .map((tx) => {
+            let isUserSender = tx.from_address === userAddr.toLowerCase()
+            return {
+              id: tx.transaction_hash,
+              type: isUserSender ? 'sent' : 'received',
+              address: isUserSender ? tx.to_address : tx.from_address,
+              quantity: tx.value,
+              pending: false,
+            }
+          })
+      }
     }
   } else {
     const assetID = getTokenAddress(tokenAddress)
@@ -141,31 +188,36 @@ const getTransactions = async (wallets, tokenAddress) => {
 
 const getTransactionDetails = async (transactionID, tokenAddress, wallets) => {
   if (tokenAddress.includes('eip155')) {
-    const ethTransaction = await chainsApi.get(
-      'v2/ethereum/mainnet/tx/' + transactionID
+    const ethTransaction = await moralisApi.get(
+      'transaction/' + transactionID,
+      {
+        chain: 'rinkeby',
+      }
     )
-    console.log(ethTransaction, 'ethTransaction')
     let rawTransaction = ethTransaction.data
-    // let userAddr = wallets.ethr.address
-    let userAddr = '0x28C6c06298d514Db089934071355E5743bf21d60'
+    console.log(rawTransaction, 'rawTransaction')
+    let userAddr = wallets.ethr.address
+    // let userAddr = '0x28C6c06298d514Db089934071355E5743bf21d60'
 
     if (rawTransaction) {
-      let transferInfo = rawTransaction.operations.native.detail
-      let isUserSender = transferInfo.from === userAddr
-      let symbol = transferInfo.currency.symbol
-      let decimal = transferInfo.currency.decimals
-      let feeSymbol = rawTransaction.operations.fee.detail.currency.symbol
+      let isUserSender = rawTransaction.from_address === userAddr.toLowerCase()
+      let symbol = SUPPORTED_TOKENS[3].symbol
+      let decimal = SUPPORTED_TOKENS[3].decimal
+      let feeSymbol = SUPPORTED_TOKENS[3].symbol
       return {
-        id: rawTransaction.id,
+        id: rawTransaction.hash,
         type: isUserSender ? 'sent' : 'received',
-        address: isUserSender ? transferInfo.to : rawTransaction.from,
-        quantity: transferInfo.value,
-        fee: rawTransaction.operations.fee.detail.value,
-        round: rawTransaction['block_id'],
-        time: rawTransaction['date'],
+        address: isUserSender
+          ? rawTransaction.to_address
+          : rawTransaction.from_address,
+        quantity: rawTransaction.value,
+        fee: rawTransaction.gas,
+        round: rawTransaction['block_number'],
+        time: rawTransaction['block_timestamp'],
         symbol,
         feeSymbol,
         decimal,
+        chain: 'ethereum',
       }
     } else {
       return {}
@@ -209,6 +261,7 @@ const getTransactionDetails = async (transactionID, tokenAddress, wallets) => {
         symbol,
         feeSymbol,
         decimal,
+        chain: 'algorand',
       }
     } else {
       return {}
@@ -216,4 +269,220 @@ const getTransactionDetails = async (transactionID, tokenAddress, wallets) => {
   }
 }
 
-export default { getAllBalances, getTransactions, getTransactionDetails }
+const getTransactionParams = async (transactionData, wallets) => {
+  if (transactionData.token.address.includes('eip155')) {
+    let fromAddress = wallets.ethr.address
+    let toAddress = transactionData.address
+
+    let input
+    if (isNativeToken(transactionData.token.address)) {
+      input = {
+        from: fromAddress,
+        to: toAddress,
+        value:
+          transactionData.amount * Math.pow(10, transactionData.token.decimal),
+      }
+    } else {
+      let tokenAddress = getTokenAddress(transactionData.token.address)
+
+      let contract = new web3.eth.Contract(minABI, tokenAddress, {
+        from: fromAddress,
+      })
+
+      input = {
+        from: fromAddress,
+        to: tokenAddress,
+        value: 0x0,
+        data: contract.methods
+          .transfer(
+            toAddress,
+            web3.utils.toWei(web3.utils.toBN(transactionData.amount))
+          )
+          .encodeABI(),
+      }
+    }
+
+    try {
+      const estimateGas = await web3.eth.estimateGas(input)
+      console.log(estimateGas, 'estimateGas getTransactionParams')
+      const params = { fee: estimateGas }
+      console.log(params, 'params getTransactionParams')
+
+      return params
+    } catch (error) {
+      console.log(error, 'error')
+    }
+  } else {
+    const params = await algodClient.getTransactionParams().do()
+    if (params.fee === 0) {
+      params.fee = 1000
+      params.flatFee = true
+    }
+
+    return params
+  }
+}
+
+const sendTransaction = async (
+  transactionData,
+  isAssetEnablingTransaction,
+  state
+) => {
+  const wallets = getWalletsData(state)
+
+  if (transactionData.token.address.includes('eip155')) {
+    let transactionParams = getTransactionParamsData(state)
+    let amount
+
+    const nonce = await web3.eth.getTransactionCount(
+      wallets.ethr.address,
+      'latest'
+    )
+
+    let transaction
+
+    if (isNativeToken(transactionData.token.address)) {
+      amount =
+        transactionData.amount * Math.pow(10, transactionData.token.decimal)
+      transaction = {
+        to: transactionData.address, // faucet address to return eth
+        value: amount,
+        gas: transactionParams.fee,
+        // maxFeePerGas: estimateGas,
+        // maxPriorityFeePerGas: estimateGas,
+        nonce: nonce,
+      }
+    } else {
+      let tokenAddress = getTokenAddress(transactionData.token.address)
+      let toAddress = transactionData.address
+      let fromAddress = wallets.ethr.address
+      amount = web3.utils.toWei(web3.utils.toBN(transactionData.amount))
+
+      let contract = new web3.eth.Contract(minABI, tokenAddress, {
+        from: fromAddress,
+      })
+
+      let input = {
+        from: fromAddress,
+        to: tokenAddress,
+        value: 0x0,
+        data: contract.methods.transfer(toAddress, amount).encodeABI(),
+      }
+      const newgasss = await web3.eth.estimateGas(input)
+      console.log(newgasss, 'newgasss')
+
+      // call transfer function
+      transaction = {
+        from: fromAddress,
+        gas: newgasss,
+        // gasPrice: web3.utils.toHex(20 * 1e9),
+        // gasLimit: web3.utils.toHex(210000),
+        to: tokenAddress,
+        value: 0x0,
+        data: contract.methods.transfer(toAddress, amount).encodeABI(),
+        nonce: nonce,
+      }
+    }
+    console.log(transaction, 'transaction')
+
+    try {
+      const signedTx = await web3.eth.accounts.signTransaction(
+        transaction,
+        wallets.ethr.privateKey.substring(2, wallets.ethr.privateKey.length)
+      )
+      console.log(signedTx, 'signedTx')
+
+      let TxHash = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+
+      console.log(TxHash, 'TxHash')
+
+      if (TxHash) {
+        const txData = {
+          id: TxHash.transactionHash,
+          amount: parseFloat(amount.toString()),
+          fee: transaction.gas,
+          to: transactionData.address,
+          from: wallets.ethr.address,
+          token: transactionData.token,
+          feeSymbol: SUPPORTED_TOKENS[3].symbol,
+        }
+
+        return txData
+      }
+    } catch (error) {
+      console.log(error, 'tx error')
+    }
+  } else {
+    let transactionParams
+    if (isAssetEnablingTransaction) {
+      transactionParams = await algodClient.getTransactionParams().do()
+    } else {
+      transactionParams = getTransactionParamsData(state)
+    }
+    let isNative = isNativeToken(transactionData.token.address)
+    let tokenAddress = getTokenAddress(transactionData.token.address)
+
+    let transaction
+
+    if (isNative) {
+      transaction = algosdk.makePaymentTxnWithSuggestedParams(
+        wallets.algo.address,
+        transactionData.address,
+        transactionData.amount * Math.pow(10, transactionData.token.decimal),
+        undefined,
+        undefined,
+        transactionParams
+      )
+    } else {
+      transaction = algosdk.makeAssetTransferTxnWithSuggestedParams(
+        wallets.algo.address,
+        isAssetEnablingTransaction
+          ? wallets.algo.address
+          : transactionData.address,
+        undefined,
+        undefined,
+        isAssetEnablingTransaction
+          ? 0
+          : transactionData.amount *
+              Math.pow(10, transactionData.token.decimal),
+        undefined,
+        parseInt(tokenAddress, 10),
+        transactionParams
+      )
+    }
+
+    const privateKey = wallets.algo.privateKey
+
+    const secretKey = Buffer.from(
+      privateKey.substring(2, privateKey.length),
+      'hex'
+    ).toJSON().data
+
+    const mnemonic = algosdk.secretKeyToMnemonic(secretKey)
+    const wallet = algosdk.mnemonicToSecretKey(mnemonic)
+
+    const signedTransaction = transaction.signTxn(wallet.sk)
+
+    const sent = await algodClient.sendRawTransaction(signedTransaction).do()
+
+    const txData = {
+      id: sent.txId,
+      amount: transaction.amount,
+      fee: transaction.fee,
+      to: transactionData.address,
+      from: wallets.algo.address,
+      token: transactionData.token,
+      feeSymbol: SUPPORTED_TOKENS[0].symbol,
+    }
+
+    return txData
+  }
+}
+
+export default {
+  getAllBalances,
+  getTransactions,
+  getTransactionDetails,
+  getTransactionParams,
+  sendTransaction,
+}
