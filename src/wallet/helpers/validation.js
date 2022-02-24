c6be1267e8ea66f3bbf40a6b83aecf1e70de1535
Saad Ibrahim <saad.lakhany@gmail.com>
2022-02-24 17:21:40 +0400

    Update validation.js

--- src/wallet/helpers/validation.js
+++ src/wallet/helpers/validation.js
@@ -1,5 +1,10 @@
 import algosdk from 'algosdk'
+import { web3 } from 'wallet/chains/ethereum'
+import { getTokenChain } from 'wallet/helpers/tokens'
 
-export const isValidWalletAddress = (address) => {
-  return algosdk.isValidAddress(address)
+export const isValidWalletAddress = (address, tokenAddress) => {
+  let chain = getTokenChain(tokenAddress)
+  return chain === 'algorand'
+    ? algosdk.isValidAddress(address)
+    : web3.utils.isAddress(address)
 }
