979304bdfed4fbf5136eaea7d1a49134cfaa237a
Saad Ibrahim <saad.lakhany@gmail.com>
2022-04-12 19:15:29 +0400

    update token helpers, add getWalletAddressForToken helper

--- src/wallet/helpers/tokens.js
+++ src/wallet/helpers/tokens.js
@@ -2,21 +2,41 @@ import { AssetId } from 'caip'
 import { utils } from 'ethers'
 
 export const getTokenAddress = (address) => {
+  if (address.includes('slip44')) {
+    return 'slip44'
+  }
+
+  if (address.includes('nep141:')) {
+    const splt = address.split('/')
+    return splt[1].replace('nep141:', '')
+  }
+
   const parsed = AssetId.parse(address)
   return parsed.assetName.reference
 }
 
 export const isNativeToken = (address) => {
   // having to put a hack for Ethereum due to CAIP library having issues, waiting for update.
-  return (
-    getTokenAddress(address) === 'slip44' ||
-    getTokenAddress(address) === '0x0000001'
-  )
+  return address.includes('slip44')
 }
 
 export const getTokenChain = (address) => {
+  if (address.includes('near:')) {
+    return 'near'
+  }
   const parsed = AssetId.parse(address)
   return parsed.chainId.namespace
+}
+
+export const getWalletAddressForToken = (tokenAddress, wallets) => {
+  const chainMapping = {
+    algorand: 'algo',
+    eip155: 'ethr',
+    near: 'near',
+  }
+
+  const chain = getTokenChain(tokenAddress)
+  return wallets[chainMapping[chain]].address
 }
 
 export const handleTokenDecimals = (quantity, decimalPlaces) => {
@@ -32,7 +52,17 @@ export const parseUnitsForSending = (quantity, decimalPlaces) => {
 }
 
 export const getExplorerUrl = (chain) => {
-  return chain === 'algorand'
-    ? 'https://testnet.algoexplorer.io/tx/'
-    : 'https://rinkeby.etherscan.io/tx/'
+  let url
+  switch (chain) {
+    case 'algorand':
+      url = 'https://testnet.algoexplorer.io/tx/'
+      break
+    case 'ethereum':
+      url = 'https://rinkeby.etherscan.io/tx/'
+      break
+    case 'near':
+      url = 'https://explorer.testnet.near.org/transactions/'
+      break
+  }
+  return url
 }
