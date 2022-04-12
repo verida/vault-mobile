52d632c647101242d5e19dbe4733d8cdb30c56cb
Saad Ibrahim <saad.lakhany@gmail.com>
2022-04-12 19:10:32 +0400

    Create near.js

---
+++ src/wallet/chains/near.js
@@ -0,0 +1,30 @@
+import * as nearAPI from 'near-api-js'
+import store from 'reduxStore'
+const { connect } = nearAPI
+const { keyStores, KeyPair } = nearAPI
+const keyStore = new keyStores.InMemoryKeyStore()
+
+// adds the keyPair you created to keyStore
+
+const config = {
+  networkId: 'testnet',
+  keyStore, // optional if not signing transactions
+  nodeUrl: 'https://rpc.testnet.near.org',
+  walletUrl: 'https://wallet.testnet.near.org',
+  helperUrl: 'https://helper.testnet.near.org',
+  explorerUrl: 'https://explorer.testnet.near.org',
+}
+
+export default async function main() {
+  const reduxState = store.getState()
+  const wallet = reduxState.wallets.data.accounts.near
+  const prvtKey = wallet.privateKey.replace('ed25519:', '')
+
+  // creates a public / private key pair using the provided private key
+  const keyPair = KeyPair.fromString(prvtKey)
+  await keyStore.setKey('testnet', wallet.address, keyPair)
+
+  const near = await connect(config)
+
+  return near
+}
