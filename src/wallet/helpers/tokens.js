db5891c8a758ed57abaaa65285916e8545effce4
Saad Ibrahim <saad.lakhany@gmail.com>
2022-04-19 04:49:27 +0400

    getTokenByAddress helper

--- src/wallet/helpers/tokens.js
+++ src/wallet/helpers/tokens.js
@@ -40,6 +40,14 @@ export const getNativeForChain = (chain) => {
   return tok
 }
 
+export const getTokenByAddress = (address) => {
+  let tok = SUPPORTED_TOKENS.find(
+    (ele) => getTokenAddress(ele.address).toLowerCase() === address
+  )
+
+  return tok
+}
+
 export const getWalletAddressForToken = (tokenAddress, wallets) => {
   const chainMapping = {
     algorand: 'algo',
