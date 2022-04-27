0e36df4df36abe4a6c2fc2cf9c97f6fb172c6787
Saad Ibrahim <saad.lakhany@gmail.com>
2022-04-27 15:16:32 +0300

    Update manage wallets screen to connect with create new wallet action

--- src/pages/Wallets/ManageWallets.js
+++ src/pages/Wallets/ManageWallets.js
@@ -1,9 +1,12 @@
-import { Container, Content, List } from 'native-base'
+import { useActionSheet } from '@expo/react-native-action-sheet'
+import { Container, Content, List, Icon } from 'native-base'
 import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
 
 import LoadingView from 'components/LoadingView'
 import NavigationHeader from 'components/Navigation/NavigationHeader'
+import { createNewWallet } from 'reduxStore/wallet/actions'
 
 import OtherSvg from '../../assets/wallets/Other.svg'
 import WalletsList from '../../components/WalletsList'
@@ -17,12 +20,30 @@ const list = [
   },
 ]
 
-export default () => {
+const ManageWallets = ({ createNewWallet }) => {
   const [loading] = useState(false)
+  const { showActionSheetWithOptions } = useActionSheet()
 
   return (
     <Container>
-      <NavigationHeader title='Wallets' />
+      <NavigationHeader
+        title='Wallets'
+        right={{
+          icon: <Icon name='add' style={{ color: '#000' }} />,
+          action: () =>
+            showActionSheetWithOptions(
+              {
+                options: ['Create new wallet', 'Cancel'],
+                cancelButtonIndex: 1,
+              },
+              (buttonIndex) => {
+                if (buttonIndex === 0) {
+                  createNewWallet()
+                }
+              }
+            ),
+        }}
+      />
       {loading ? (
         <LoadingView />
       ) : (
@@ -39,3 +60,15 @@ export default () => {
 const styles = StyleSheet.create({
   content: { backgroundColor: SNOW_COLOR, paddingVertical: 25 },
 })
+
+const mapStateToProps = (state) => {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createNewWallet: () => dispatch(createNewWallet()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageWallets)
