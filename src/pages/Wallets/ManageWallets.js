fe79b772fbd50460f0149381617915f752cc0484
Saad Ibrahim <saad.lakhany@gmail.com>
2022-04-29 14:58:40 +0300

    add selected item support to manage wallet

--- src/pages/Wallets/ManageWallets.js
+++ src/pages/Wallets/ManageWallets.js
@@ -1,28 +1,43 @@
 import { useActionSheet } from '@expo/react-native-action-sheet'
+import * as SecureStore from 'expo-secure-store'
 import { Container, Content, List, Icon } from 'native-base'
 import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
+import { SELECTED_WALLET_STORAGE_KEY } from 'api/AccountManager'
 
 import LoadingView from 'components/LoadingView'
 import NavigationHeader from 'components/Navigation/NavigationHeader'
-import { createNewWallet } from 'reduxStore/wallet/actions'
+import { createNewWallet, setSelectedWallet } from 'reduxStore/wallet/actions'
+import {
+  getAllWallets,
+  getSelectedWallet,
+  getWalletProcessingState,
+} from 'reduxStore/wallet/selectors'
 
 import OtherSvg from '../../assets/wallets/Other.svg'
 import WalletsList from '../../components/WalletsList'
 import { SNOW_COLOR } from '../../constants/color'
 
-const list = [
-  {
-    label: 'Multichain Wallet',
-    icon: <OtherSvg />,
-    count: 3,
-  },
-]
-
-const ManageWallets = ({ createNewWallet }) => {
-  const [loading] = useState(false)
+const ManageWallets = ({
+  wallets,
+  createNewWallet,
+  setSelectedWallet,
+  navigation,
+  selectedWalletId,
+  loading,
+}) => {
   const { showActionSheetWithOptions } = useActionSheet()
+
+  const list = Object.values(wallets).map((singleWallet) => {
+    const { label, id, accounts } = singleWallet
+    return {
+      label,
+      id,
+      icon: <OtherSvg />,
+      count: Object.keys(accounts).length,
+    }
+  })
 
   return (
     <Container>
@@ -49,7 +64,35 @@ const ManageWallets = ({ createNewWallet }) => {
       ) : (
         <Content style={styles.content}>
           <List>
-            <WalletsList list={list} />
+            <WalletsList
+              onPressItem={(item) => {
+                showActionSheetWithOptions(
+                  {
+                    options: [
+                      'Show chain addresses',
+                      'Switch to this wallet',
+                      'Cancel',
+                    ],
+                    cancelButtonIndex: 2,
+                  },
+                  (buttonIndex) => {
+                    if (buttonIndex === 0) {
+                      navigation.navigate('SingleWallet', { item })
+                    }
+                    if (buttonIndex === 1) {
+                      let selectedWalletID = item.id
+                      setSelectedWallet(selectedWalletID)
+                      SecureStore.setItemAsync(
+                        SELECTED_WALLET_STORAGE_KEY,
+                        selectedWalletID
+                      )
+                    }
+                  }
+                )
+              }}
+              list={list}
+              selectedWalletId={selectedWalletId}
+            />
           </List>
         </Content>
       )}
@@ -62,12 +105,17 @@ const styles = StyleSheet.create({
 })
 
 const mapStateToProps = (state) => {
-  return {}
+  return {
+    wallets: getAllWallets(state),
+    selectedWalletId: getSelectedWallet(state),
+    loading: getWalletProcessingState(state),
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     createNewWallet: () => dispatch(createNewWallet()),
+    setSelectedWallet: (walletID) => dispatch(setSelectedWallet(walletID)),
   }
 }
 
