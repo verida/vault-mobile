90538bcef5ff92625bb448636821777eabc42fec
Saad Ibrahim <saad.lakhany@gmail.com>
2022-05-11 18:28:08 +0300

    add import support to manage wallets screen

--- src/pages/Wallets/ManageWallets.js
+++ src/pages/Wallets/ManageWallets.js
@@ -1,8 +1,8 @@
 import { useActionSheet } from '@expo/react-native-action-sheet'
 import * as SecureStore from 'expo-secure-store'
 import { Container, Content, Icon, List } from 'native-base'
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 import { connect } from 'react-redux'
 
 import { SELECTED_WALLET_STORAGE_KEY } from 'api/AccountManager'
@@ -18,6 +18,7 @@ import {
 import OtherSvg from '../../assets/wallets/Other.svg'
 import WalletsList from '../../components/WalletsList'
 import { SNOW_COLOR } from '../../constants/color'
+import ImportWalletModal from './ImportWalletModal'
 
 const ManageWallets = ({
   wallets,
@@ -26,8 +27,14 @@ const ManageWallets = ({
   navigation,
   selectedWalletId,
   loading,
+  onImportWallet,
 }) => {
   const { showActionSheetWithOptions } = useActionSheet()
+  const [importModalVisible, setImportModalVisible] = useState(false)
+
+  const onPressImportWallet = () => {
+    setImportModalVisible(true)
+  }
 
   const list = Object.values(wallets).map((singleWallet) => {
     const { label, id, accounts } = singleWallet
@@ -48,12 +55,15 @@ const ManageWallets = ({
           action: () =>
             showActionSheetWithOptions(
               {
-                options: ['Create new wallet', 'Cancel'],
-                cancelButtonIndex: 1,
+                options: ['Create new wallet', 'Import a wallet', 'Cancel'],
+                cancelButtonIndex: 2,
               },
               (buttonIndex) => {
                 if (buttonIndex === 0) {
                   onCreateNewWallet()
+                }
+                if (buttonIndex === 1) {
+                  onPressImportWallet()
                 }
               }
             ),
@@ -62,39 +72,46 @@ const ManageWallets = ({
       {loading ? (
         <LoadingView />
       ) : (
-        <Content style={styles.content}>
-          <List>
-            <WalletsList
-              onPressItem={(item) => {
-                showActionSheetWithOptions(
-                  {
-                    options: [
-                      'Manage Wallet',
-                      'Switch to this wallet',
-                      'Cancel',
-                    ],
-                    cancelButtonIndex: 2,
-                  },
-                  (buttonIndex) => {
-                    if (buttonIndex === 0) {
-                      navigation.navigate('SingleWallet', { item })
+        <View style={{ flex: 1 }}>
+          <Content style={styles.content}>
+            <List>
+              <WalletsList
+                onPressItem={(item) => {
+                  showActionSheetWithOptions(
+                    {
+                      options: [
+                        'Manage Wallet',
+                        'Switch to this wallet',
+                        'Cancel',
+                      ],
+                      cancelButtonIndex: 2,
+                    },
+                    (buttonIndex) => {
+                      if (buttonIndex === 0) {
+                        navigation.navigate('SingleWallet', { item })
+                      }
+                      if (buttonIndex === 1) {
+                        let selectedWalletID = item.id
+                        onSetSelectedWallet(selectedWalletID)
+                        SecureStore.setItemAsync(
+                          SELECTED_WALLET_STORAGE_KEY,
+                          selectedWalletID
+                        )
+                      }
                     }
-                    if (buttonIndex === 1) {
-                      let selectedWalletID = item.id
-                      onSetSelectedWallet(selectedWalletID)
-                      SecureStore.setItemAsync(
-                        SELECTED_WALLET_STORAGE_KEY,
-                        selectedWalletID
-                      )
-                    }
-                  }
-                )
-              }}
-              list={list}
-              selectedWalletId={selectedWalletId}
-            />
-          </List>
-        </Content>
+                  )
+                }}
+                list={list}
+                selectedWalletId={selectedWalletId}
+              />
+            </List>
+          </Content>
+          <ImportWalletModal
+            hideModal={() => setImportModalVisible(false)}
+            visible={importModalVisible}
+            onImportWallet={onImportWallet}
+          />
+        </View>
       )}
     </Container>
   )
@@ -116,6 +133,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     onCreateNewWallet: () => dispatch(createNewWallet()),
     onSetSelectedWallet: (walletID) => dispatch(setSelectedWallet(walletID)),
+    onImportWallet: (args) => dispatch(createNewWallet(args)),
   }
 }
 
