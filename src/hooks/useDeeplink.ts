5f28e20be00a2b5440743e0942e40752f94f492e
Saad Ibrahim <saad.lakhany@gmail.com>
2022-06-07 01:28:56 +0400

    try fixing deeplink hook typescript error

--- src/hooks/useDeeplink.ts
+++ src/hooks/useDeeplink.ts
@@ -1,12 +1,17 @@
+import { CompositeScreenProps } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 import * as Sentry from '@sentry/react-native'
 import parse from 'url-parse'
 
-import { MainStackParams } from 'navigation/types'
+import { MainStackParams, DashboardTabParams } from 'navigation/types'
 
-export function useDeeplink(
-  navigation: NativeStackNavigationProp<MainStackParams, keyof MainStackParams>
-) {
+type NavProp = CompositeScreenProps<
+  BottomTabNavigationProp<DashboardTabParams, keyof DashboardTabParams>,
+  NativeStackNavigationProp<MainStackParams, keyof MainStackParams>
+>
+
+export function useDeeplink(navigation: NavProp) {
   return function (url: string) {
     try {
       const parsedUrl = parse(url, true)
