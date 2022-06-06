f352ba94b4dbadfbe0d888d4c4399dc222550924
Saad Ibrahim <saad.lakhany@gmail.com>
2022-06-07 01:03:44 +0400

    add ts support to SuccessFailure.js

--- src/pages/Wallets/SuccessFailure.tsx
+++ src/pages/Wallets/SuccessFailure.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 import Button from 'components/Button'
 import Layout from 'components/Layouts/Layout'
@@ -9,8 +10,15 @@ import FailureCross from '../../assets/failure_cross.svg'
 import SuccessTick from '../../assets/success_tick.svg'
 import { BLACK_COLOR } from '../../constants/color'
 import { NUNITO_SANS_BOLD } from '../../constants/text'
+import { MainStackParams } from 'navigation/types'
 
-export default ({ navigation, route }) => {
+type Props = {
+  navigation: NativeStackNavigationProp<MainStackParams>
+  route: any
+}
+
+export default (props: Props) => {
+  const { navigation, route } = props
   const { failure } = route.params
   const icon = failure ? <FailureCross /> : <SuccessTick />
   const titleText = failure ? 'Ooops..' : 'Success!'
