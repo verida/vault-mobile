05a3c19f51a3fd7439e6f7377c735b366b028fbe
Saad Ibrahim <saad.lakhany@gmail.com>
2022-06-27 18:56:31 +0300

    add token selectors

---
+++ src/reduxStore/tokens/selectors.js
@@ -0,0 +1,44 @@
+import { createSelector } from 'reselect'
+
+export const selectTokensData = (state) => state.tokens || {}
+
+export const selectTokensTimestamp = (state) => {
+  const tokensData = selectTokensData(state)
+  return tokensData.timeFetched
+}
+
+export const selectRawTokens = (state) => {
+  const tokensData = selectTokensData(state)
+  return tokensData.data
+}
+
+export const selectTokens = (state) => {
+  const rawTokens = selectRawTokens(state)
+  // console.log(rawTokens, 'rawTokens')
+  const list = []
+  if (rawTokens) {
+    rawTokens.forEach((singleToken) => {
+      const { tokens, ...chainToken } = singleToken
+
+      list.push(chainToken)
+
+      Object.values(tokens).forEach((singleSubToken) => {
+        list.push(singleSubToken)
+      })
+    })
+  }
+  return list
+}
+
+export const selectChains = (state) => {
+  const rawTokens = selectRawTokens(state)
+  // console.log(rawTokens, 'rawTokens')
+  const list = {}
+  if (rawTokens) {
+    rawTokens.forEach((singleToken) => {
+      list[singleToken.asset.chainId.namespace] = singleToken.asset.chainId
+    })
+  }
+
+  return list
+}
