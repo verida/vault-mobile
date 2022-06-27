49a6c7cdffa62fdeae8eec3d41b974dc1745a810
Saad Ibrahim <saad.lakhany@gmail.com>
2022-06-27 19:00:17 +0300

    Update Dashboard.js

--- src/pages/Tokens/Dashboard.js
+++ src/pages/Tokens/Dashboard.js
@@ -9,33 +9,25 @@ import NavigationHeader from 'components/Navigation/NavigationHeader'
 import TestnetWarning from 'components/Tokens/TestnetWarning'
 import TokenBanner from 'components/Tokens/TokenBanner'
 import TokensList from 'components/Tokens/TokensList'
-import { getBalances, getPrices } from 'reduxStore/wallet/actions'
+import { getBalances } from 'reduxStore/wallet/actions'
 import { getTokensData, getWalletsData } from 'reduxStore/wallet/selectors'
 
 import SendListModal from './SendListModal'
 
-const TokenDashboard = ({
-  navigation,
-  onGetPrices,
-  onGetBalances,
-  data,
-  wallets,
-}) => {
+const TokenDashboard = ({ navigation, onGetBalances, data, wallets }) => {
   const [sendModalVisible, setSendModalVisible] = useState(false)
 
-  function pullToRefresh() {
+  async function pullToRefresh() {
     onGetBalances()
-    onGetPrices()
   }
 
   useEffect(() => {
     async function loadData() {
       onGetBalances()
-      onGetPrices()
     }
 
     loadData()
-  }, [onGetBalances, onGetPrices, wallets])
+  }, [onGetBalances, wallets])
 
   const { loading, listAndTotal } = data
 
@@ -91,13 +83,12 @@ const mapStateToProps = (rootState) => {
   const state = rootState.main
   return {
     wallets: getWalletsData(state),
-    data: getTokensData(state),
+    data: getTokensData(rootState),
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onGetPrices: () => dispatch(getPrices()),
     onGetBalances: () => dispatch(getBalances()),
   }
 }
