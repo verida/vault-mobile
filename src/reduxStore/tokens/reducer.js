31aaab4261b54a98178acf182aced64d801d372b
Saad Ibrahim <saad.lakhany@gmail.com>
2022-06-27 18:56:37 +0300

    add token reducer

---
+++ src/reduxStore/tokens/reducer.js
@@ -0,0 +1,44 @@
+import {
+  TOKENS_FETCH_FAILED,
+  TOKENS_FETCH_START,
+  FETCHED_TOKENS,
+} from './types'
+
+const initialState = {
+  data: [],
+  fetching: false,
+  error: undefined,
+  timeFetched: undefined,
+}
+
+export const tokensReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOKENS_FETCH_START:
+      return {
+        ...state,
+        fetching: true,
+        error: undefined,
+        data: [],
+        timeFetched: undefined,
+      }
+    case FETCHED_TOKENS:
+      return {
+        ...state,
+        fetching: false,
+        error: undefined,
+        data: action.data,
+        timeFetched: Date.now(),
+      }
+    case TOKENS_FETCH_FAILED:
+      return {
+        ...state,
+        fetching: false,
+        error: action.error,
+        data: [],
+        timeFetched: undefined,
+      }
+
+    default:
+      return state
+  }
+}
