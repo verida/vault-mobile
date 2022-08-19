91ace4b18cbc6e5b6e126c22f4d5c3a968d732eb
Saad Ibrahim <saad.lakhany@gmail.com>
2022-08-19 17:49:48 +0400

    update token helpers

--- src/wallet/helpers/tokens.js
+++ src/wallet/helpers/tokens.js
@@ -1,4 +1,4 @@
-import WalletUtils from '@verida/wallet-utils'
+import multiChainWallet from 'wallet/helpers/multiChainWallet'
 import { AssetId } from 'caip'
 import { utils } from 'ethers'
 
@@ -28,27 +28,14 @@ export const getTokenChainReference = (address) => {
 
 export const getNativeForChain = (tokens, chain) => {
   let tok = tokens.find(
-    (ele) =>
-      ele.asset.chainId.namespace + ':' + ele.asset.chainId.reference ===
-        chain && ele.asset.assetName.namespace === 'slip44'
+    (ele) => ele.slug === chain && ele.asset.assetName.namespace === 'slip44'
   )
 
   return tok
 }
 
-export const getChainMapping = (chain) => {
-  const chainMapping = {
-    'algorand:SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=': 'algo',
-    'eip155:4': 'ethr',
-    'near:testnet': 'near',
-    'eip155:80001': 'poly',
-  }
-
-  return chainMapping[chain]
-}
-
 export const getWalletAddressForToken = (chain, wallets) => {
-  return wallets[getChainMapping(chain)].address
+  return wallets[chain].address
 }
 
 export const handleTokenDecimals = (quantity, decimalPlaces) => {
@@ -63,31 +50,12 @@ export const parseUnitsForSending = (quantity, decimalPlaces) => {
   return utils.parseUnits(quantity, decimalPlaces)
 }
 
-export const getExplorerUrl = (chainId) => {
-  let url
-  switch (chainId.namespace) {
-    case 'algorand':
-      url = 'https://testnet.algoexplorer.io/tx/'
-      break
-    case 'eip155':
-      url =
-        chainId.reference === '4'
-          ? 'https://rinkeby.etherscan.io/tx/'
-          : 'https://mumbai.polygonscan.com/tx/'
-      break
-    case 'near':
-      url = 'https://explorer.testnet.near.org/transactions/'
-      break
-  }
-  return url
-}
-
-export const rawDataToReduxState = (walletData) => {
+export const rawDataToReduxState = (walletData, chains) => {
   let wallets = {}
   walletData.forEach((walt) => {
     let mnemonic = walt.mnemonic
     let waltId = walt._id
-    let accounts = WalletUtils.MultiChainWallet.generateHDWallets(mnemonic)
+    let accounts = multiChainWallet.generateWalletsForChains(mnemonic, chains)
 
     wallets[waltId] = {
       seedPhrase: mnemonic,
@@ -100,10 +68,8 @@ export const rawDataToReduxState = (walletData) => {
   return wallets
 }
 
-export const getWalletAddressForAsset = (asset, wallets) => {
-  const chain = getTokenChain(asset)
-  const chainRef = getTokenChainReference(asset)
-  return wallets[getChainMapping(chain + ':' + chainRef)].address
+export const getWalletAddressForAsset = (chainSlug, wallets) => {
+  return wallets[chainSlug].address
 }
 
 export const tokenCaipObjectToString = (asset) => {
