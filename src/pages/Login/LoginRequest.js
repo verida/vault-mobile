4307d4db88d5ba3d9f3b3f030abe3d84aeca53c6
andy-verida <andy@verida.io>
2022-10-21 19:02:34 +0700

    Get WalletConnect info from Verida SSO login context and connect to wallet

--- src/pages/Login/LoginRequest.js
+++ src/pages/Login/LoginRequest.js
@@ -13,6 +13,7 @@ import CustomFooter from 'components/Layouts/CustomFooter'
 import LoadingView from 'components/LoadingView'
 import NavigationHeader from 'components/Navigation/NavigationHeader'
 import Text from 'components/Text'
+import { useWalletConnect, useWalletConnectv2 } from 'hooks/useWalletConnect'
 
 import MobileSvg from '../../assets/mobile.svg'
 import Button from '../../components/Button'
@@ -34,6 +35,9 @@ export default (props) => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [ws, setWebsocket] = useState(null)
   const [expired, setExpired] = useState(false)
+
+  const { requestConnect } = useWalletConnect()
+  const { requestConnect: requestConnectv2 } = useWalletConnectv2()
 
   useEffect(() => {
     const init = async () => {
@@ -90,6 +94,7 @@ export default (props) => {
                 keyBytes
               )
               const parsed = JSON.parse(decrypted)
+              console.log('Login info', parsed)
               setInfo({
                 request,
                 payload,
@@ -97,6 +102,7 @@ export default (props) => {
                 key,
                 logoUrl: parsed.logoUrl,
                 openUrl: parsed.openUrl ? parsed.openUrl : null,
+                walletConnect: parsed.walletConnect,
               })
               setStatus('loaded')
             } catch (e) {
@@ -222,6 +228,11 @@ export default (props) => {
         const jsonEncoded = JSON.stringify(response)
         const encoded = Buffer.from(jsonEncoded).toString('base64')
         await Linking.openURL(info.openUrl + '?_verida_auth=' + encoded)
+      }
+
+      console.log('Info', info, info.walletConnect)
+      if (info.walletConnect?.uri) {
+        requestConnect(info.walletConnect.uri)
       }
 
       await saveLoginRequest(true)
