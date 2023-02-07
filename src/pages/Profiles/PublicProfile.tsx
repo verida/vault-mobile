48d73497aab23ac9155c5c56b3d92e82b067d2d2
andy-verida <andy@verida.io>
2023-02-07 20:36:26 +0700

    Fix typo

--- src/pages/Profiles/PublicProfile.tsx
+++ src/pages/Profiles/PublicProfile.tsx
@@ -519,7 +519,7 @@ const PublicProfile = ({ publicProfileData, updatePublicProfileData }: any) => {
             )}
             <Text style={[styles.description, { marginVertical: 0 }]}>
               On your Verida One page we show your wallet addresses with their
-              public titles and the assets related to them (collectibles,
+              public lables and the assets related to them (collectibles,
               badges, etc)
             </Text>
 
