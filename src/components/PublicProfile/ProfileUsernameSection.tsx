10e3d64e3b3599f04f4cc5c8e583ac4baa3ff624
andy-verida <andy@verida.io>
2023-04-19 12:22:28 +0700

    Remove border radius from image

--- src/components/PublicProfile/ProfileUsernameSection.tsx
+++ src/components/PublicProfile/ProfileUsernameSection.tsx
@@ -35,135 +35,142 @@ export const ProfileUsernameSection = ({ did, username }: Props) => {
   const buildUrl = () => `${VERIDA_ONE_WEBSITE}${username || did}`
 
   return (
-    <ImageBackground
-      resizeMode='stretch'
-      source={require('assets/profile_link_bg.png')}
-      style={styles.oneProfileLinkContainer}>
-      <TouchableOpacity
-        onPress={() => {
-          Clipboard.setString(buildUrl())
-          Snackbar.show({
-            text: 'Copied to clipboard',
-            duration: Snackbar.LENGTH_SHORT,
-          })
-        }}>
+    <View style={styles.oneProfileLinkContainer}>
+      <ImageBackground
+        resizeMode='stretch'
+        source={require('assets/profile_link_bg.png')}
+        style={styles.background}>
+        <TouchableOpacity
+          onPress={() => {
+            Clipboard.setString(buildUrl())
+            Snackbar.show({
+              text: 'Copied to clipboard',
+              duration: Snackbar.LENGTH_SHORT,
+            })
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginHorizontal: 28,
+            }}>
+            <SubHeadline
+              numberOfLines={1}
+              ellipsizeMode='tail'
+              style={{
+                color: theme.color.onPrimary,
+                marginRight: theme.spacing.xs,
+              }}>{`verida.one/${username || did}`}</SubHeadline>
+            <Icon name='copy' color={theme.color.onPrimary} size={16} />
+          </View>
+        </TouchableOpacity>
         <View
           style={{
             flexDirection: 'row',
-            alignItems: 'center',
-            marginHorizontal: 28,
+            width: '100%',
+            justifyContent: 'space-between',
+            marginTop: theme.spacing.l,
           }}>
-          <SubHeadline
-            numberOfLines={1}
-            ellipsizeMode='tail'
-            style={{
+          <Button
+            textStyle={{
+              fontSize: theme.fontSize.m,
               color: theme.color.onPrimary,
-              marginRight: theme.spacing.xs,
-            }}>{`verida.one/${username || did}`}</SubHeadline>
-          <Icon name='copy' color={theme.color.onPrimary} size={16} />
+            }}
+            style={styles.roundedButton}
+            color='transparent'
+            onPress={() => {
+              Linking.openURL(buildUrl())
+            }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text
+                style={{
+                  fontSize: theme.fontSize.m,
+                  fontWeight: '700',
+                  color: theme.color.onPrimary,
+                  marginRight: theme.spacing.s,
+                }}>
+                Go to
+              </Text>
+              <Icon name='goto' color={theme.color.onPrimary} size={16} />
+            </View>
+          </Button>
+          <Button
+            textStyle={{
+              fontSize: theme.fontSize.m,
+              color: theme.color.onPrimary,
+            }}
+            style={[styles.roundedButton, { marginLeft: theme.spacing.m }]}
+            color='transparent'
+            onPress={() => {
+              Share.share({
+                title: 'Verida One',
+                message: `My profile ${buildUrl()}`,
+                url: buildUrl(),
+              })
+            }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text
+                style={{
+                  fontSize: theme.fontSize.m,
+                  fontWeight: '700',
+                  color: theme.color.onPrimary,
+                  marginRight: theme.spacing.s,
+                }}>
+                Share
+              </Text>
+              <Icon name='share' color={theme.color.onPrimary} size={16} />
+            </View>
+          </Button>
         </View>
-      </TouchableOpacity>
-      <View
-        style={{
-          flexDirection: 'row',
-          width: '100%',
-          justifyContent: 'space-between',
-          marginTop: theme.spacing.l,
-        }}>
-        <Button
-          textStyle={{
-            fontSize: theme.fontSize.m,
-            color: theme.color.onPrimary,
-          }}
-          style={styles.roundedButton}
-          color='transparent'
-          onPress={() => {
-            Linking.openURL(buildUrl())
-          }}>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Text
-              style={{
-                fontSize: theme.fontSize.m,
-                fontWeight: '700',
-                color: theme.color.onPrimary,
-                marginRight: theme.spacing.s,
-              }}>
-              Go to
-            </Text>
-            <Icon name='goto' color={theme.color.onPrimary} size={16} />
-          </View>
-        </Button>
-        <Button
-          textStyle={{
-            fontSize: theme.fontSize.m,
-            color: theme.color.onPrimary,
-          }}
-          style={[styles.roundedButton, { marginLeft: theme.spacing.m }]}
-          color='transparent'
-          onPress={() => {
-            Share.share({
-              title: 'Verida One',
-              message: `My profile ${buildUrl()}`,
-              url: buildUrl(),
-            })
-          }}>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Text
-              style={{
-                fontSize: theme.fontSize.m,
-                fontWeight: '700',
-                color: theme.color.onPrimary,
-                marginRight: theme.spacing.s,
-              }}>
-              Share
-            </Text>
-            <Icon name='share' color={theme.color.onPrimary} size={16} />
-          </View>
-        </Button>
-      </View>
-      {username ? null : (
-        <View style={styles.card}>
-          <View style={{ flex: 1 }}>
-            <SubHeadline style={{ color: theme.color.onPrimary }}>
-              Claim your unique username now!
-            </SubHeadline>
-            <Text style={{ color: theme.color.onPrimary }}>
-              Secure your identity with a personalized username for easier
-              sharing and increased privacy
-            </Text>
-          </View>
-          <View style={{ flex: 1 }}>
-            <Image
-              style={{
-                marginLeft: 20,
-                marginBottom: theme.spacing.m,
-                width: 111,
-                height: 120,
-              }}
-              source={require('assets/username_placehoder.png')}
-            />
+        {username ? null : (
+          <View style={styles.card}>
+            <View style={{ flex: 1 }}>
+              <SubHeadline style={{ color: theme.color.onPrimary }}>
+                Claim your unique username now!
+              </SubHeadline>
+              <Text style={{ color: theme.color.onPrimary }}>
+                Secure your identity with a personalized username for easier
+                sharing and increased privacy
+              </Text>
+            </View>
+            <View style={{ flex: 1 }}>
+              <Image
+                style={{
+                  marginLeft: 20,
+                  marginBottom: theme.spacing.m,
+                  width: 111,
+                  height: 120,
+                }}
+                source={require('assets/username_placehoder.png')}
+              />
 
-            <Button
-              textStyle={{
-                fontSize: theme.fontSize.m,
-                color: theme.color.onPrimary,
-              }}
-              style={styles.capsuleButtton}
-              color='transparent'
-              onPress={() => navigation.navigate('ClaimUsername')}>
-              Claim username
-            </Button>
+              <Button
+                textStyle={{
+                  fontSize: theme.fontSize.m,
+                  color: theme.color.onPrimary,
+                }}
+                style={styles.capsuleButtton}
+                color='transparent'
+                onPress={() => navigation.navigate('ClaimUsername')}>
+                Claim username
+              </Button>
+            </View>
           </View>
-        </View>
-      )}
-    </ImageBackground>
+        )}
+      </ImageBackground>
+    </View>
   )
 }
 
 const createStyles = (theme: Theme) =>
   StyleSheet.create({
     oneProfileLinkContainer: {
-      position: 'relative',
+      borderRadius: theme.roundness.s,
+      borderColor: theme.color.grey300,
+      borderWidth: 1,
+      overflow: 'hidden',
+    },
+    background: {
       minHeight: 140,
       flexDirection: 'column',
       alignItems: 'center',
