858825975f2e3cc047a0bdfa8cdec27a3ceb4310
andy-verida <andy@verida.io>
2023-04-24 19:14:01 +0700

    Fix Android build issue; Implement a temporary way to copy www folder to Android document dir

--- src/features/polygonid/verida/hooks/useInstallWebView.ts
+++ src/features/polygonid/verida/hooks/useInstallWebView.ts
@@ -1,7 +1,29 @@
 import * as React from 'react'
 import RNBlobUtil from 'react-native-blob-util'
+import RNFS from 'react-native-fs'
 
 import { Stateful } from '../../@types'
+import { Platform } from 'react-native'
+
+const copyAssetWebFolderToDocument = async (fromDir: string, toDir: string) => {
+  const reader = await RNFS.readDirAssets(fromDir);
+
+  const directories = reader.filter((item) => item.isDirectory());
+  await Promise.all(directories.map(async (directory) => {
+    await RNFS.mkdir(`${toDir}/${directory.path}`)
+  }))
+  
+  await Promise.all(reader.filter((item) => item.isFile()).map(async (file) => {
+    await RNFS.copyFileAssets(file.path, `${toDir}/${file.path}`)
+  }))
+
+  const directioriesFilesPromises = directories.map((dir) => (
+    copyAssetWebFolderToDocument(dir.path, toDir)
+  ));
+
+  await Promise.all(directioriesFilesPromises);
+};
+
 
 const loadingState = (): Stateful<string> => ({
   loading: true,
@@ -9,7 +31,10 @@ const loadingState = (): Stateful<string> => ({
 
 export function useInstallWebView({
   // Static Bundle Directory
-  fromDir = `${RNBlobUtil.fs.dirs.MainBundleDir}/www`,
+  fromDir = Platform.select({
+    'android': 'www',
+    default: `${RNBlobUtil.fs.dirs.MainBundleDir}/www`
+  }),
   // Target Directory
   toDir = `${RNBlobUtil.fs.dirs.DocumentDir}/verida`,
 }: {
@@ -23,16 +48,33 @@ export function useInstallWebView({
       try {
         setState(loadingState)
 
-        if (!(await RNBlobUtil.fs.exists(fromDir)))
-          throw new Error(`Failed to install from "${fromDir}".`)
+        if (Platform.OS === 'ios') {
+          if (!(await RNBlobUtil.fs.exists(fromDir)))
+            throw new Error(`Failed to install from "${fromDir}".`)
 
-        if (await RNBlobUtil.fs.exists(toDir)) {
-          console.log('Directory already installed. Skipping installation...')
-          return setState({ result: toDir, loading: false })
+          if (await RNBlobUtil.fs.exists(toDir)) {
+            console.log('Directory already installed. Skipping installation...')
+            return setState({ result: toDir, loading: false })
+          }
+
+          console.log('Installing WebView.')
+          await RNBlobUtil.fs.cp(fromDir, toDir)
+        } else if (Platform.OS === 'android') {
+          const webAsset = RNBlobUtil.fs.asset('www/index.html')
+          if (!webAsset) {
+            throw new Error(`Failed to install from "${fromDir}".`)
+          }
+
+          if (await RNBlobUtil.fs.exists(`${toDir}/www`)) {
+            console.log('Directory already installed. Skipping installation...')
+            return setState({ result: toDir, loading: false })
+          }
+
+          console.log('Installing WebView.')
+          await RNFS.mkdir(`${toDir}/www`)
+          await copyAssetWebFolderToDocument(fromDir, toDir)
         }
 
-        console.log('Installing WebView.')
-        await RNBlobUtil.fs.cp(fromDir, toDir)
 
         if (__DEV__) {
           console.log('Directory content:')
