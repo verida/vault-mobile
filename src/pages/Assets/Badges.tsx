aec3cce5c77ca43ba96f197132b94ee2e5805289
andy-verida <andy@verida.io>
2023-06-07 18:34:59 +0700

    Some improvements

--- src/pages/Assets/Badges.tsx
+++ src/pages/Assets/Badges.tsx
@@ -1,53 +1,27 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useNavigation } from '@react-navigation/native'
-import * as sentry from '@sentry/react-native'
 import {
   Canvas,
-  Circle,
   CornerPathEffect,
+  DashPathEffect,
   Group,
   Image as SkiaImage,
   Path,
+  RadialGradient,
+  Rect,
   Skia,
   useImage,
+  vec,
 } from '@shopify/react-native-skia'
 import { useTheme } from 'contexts/ThemeContext'
-import { getNFTImageUri } from 'helpers/nft'
-import React, { useCallback, useEffect } from 'react'
-import {
-  ListRenderItem,
-  RefreshControl,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-  useWindowDimensions,
-  View,
-} from 'react-native'
-import FastImage from 'react-native-fast-image'
-import { useDispatch, useSelector } from 'react-redux'
-import { VeridaWallet } from 'types/wallet'
+import React from 'react'
+import { ScrollView, StyleSheet, useWindowDimensions, View } from 'react-native'
+import { useDispatch } from 'react-redux'
 
-import { NFT, NFTCollection, NFTMetadata } from 'api/types'
-import NFTPlaceholder from 'assets/stubs/nft_placeholder.svg'
-import { NftItem } from 'components/Assets/NftItem'
-import Button from 'components/Button'
-import Container from 'components/Container'
-import GridView from 'components/Grids/GridView'
 import { Line } from 'components/Line'
-import LoadingIndicator from 'components/LoadingIndicator'
-import { Tag } from 'components/Tag'
-import { Title } from 'components/Typography/Title'
-import { useReduxState } from 'hooks/useReduxState'
 import { useThemeAwareStyle } from 'hooks/useThemeAwareStyle'
-import { useGetWalletNFTCollectionsQuery } from 'reduxStore/assets/api'
-import {
-  allWalletsSelector,
-  getUniqueWalletAddresses,
-  selectedWalletSelector,
-} from 'reduxStore/wallet/selectors'
 import { Theme } from 'styles/types'
 
-import { IMAGE_WIDTH, NUMBER_OF_COLUMNS } from './constants'
+import { IMAGE_WIDTH } from './constants'
 
 const imageSize = 160
 function makeHexagonPath(size, offset) {
@@ -66,37 +40,52 @@ function makeHexagonPath(size, offset) {
     [-halfed, -sqrt], //e
     [halfed, -sqrt], //f
   ].map(([x, y]) => [x + xOffset, y + yOffset])
-  console.log(points)
+  // console.log(points)
   path.moveTo(...points[0])
 
-  points.forEach((point) => path.lineTo(...point))
+  points.slice(1, 6).forEach((point) => path.lineTo(...point))
   path.close()
   return path
 }
 
-export function ClipImage({ imageSrc, path, offsetX, offsetY }) {
+export function ClipImage({
+  imageSrc,
+  path,
+  innerPath,
+  color,
+  offsetX,
+  offsetY,
+}) {
   const image = useImage(imageSrc)
-  const hexagonSize = imageSize / 2
-  // const path = makeHexagonPath(hexagonSize, [hexagonSize, hexagonSize])
-  if (!image) return null
   return (
-    <Group clip={path}>
+    <Group clip={image ? path : null}>
       <Path
         path={path}
-        color='transparent'
+        color={color}
+        style='stroke'
+        strokeJoin='round'
+        strokeWidth={0.5}>
+        <CornerPathEffect r={14} />
+      </Path>
+      <Path
+        path={innerPath}
+        color={'blue'}
         style='stroke'
         strokeJoin='round'
         strokeWidth={1}>
+        <DashPathEffect intervals={[8, 8]} />
         <CornerPathEffect r={14} />
       </Path>
-      <SkiaImage
-        image={image}
-        fit='cover'
-        x={offsetX}
-        y={offsetY}
-        width={imageSize + 24}
-        height={imageSize + 24}
-      />
+      {image && (
+        <SkiaImage
+          image={image}
+          fit='cover'
+          x={offsetX}
+          y={offsetY}
+          width={imageSize + 25}
+          height={imageSize + 25}
+        />
+      )}
     </Group>
   )
 }
@@ -123,19 +112,63 @@ const Badges = () => {
       'Grid',
       JSON.stringify({ numberOfCols, numberOfRows, colOffset, rowOffset })
     )
-    let imgIndex = 0
-    const images = [
-      'https://ipfs.moralis.io:2053/ipfs/QmbPHjLsp48QuSoCtDHvXRnfzAqRDYsX4udQXRQ5gB2w87/Gen0/verida-identity.png',
-      'https://ipfs.moralis.io:2053/ipfs/QmQsghP7Y1dbZHgoo48hQz8SHdFk2mjmPikhkHg69HHhWb/Gen0/discord-account.png',
-      'https://ipfs.moralis.io:2053/ipfs/QmT1hqphvztijTtEKd1xdUMtPXFfKqrx7qRby3mpeLeiMQ/Gen0/facebook-account.png',
-      'https://ipfs.moralis.io:2053/ipfs/QmVZYkMfNC26LcwRJPdZmfUVGs5bmGFxz6AFpwwrAcVuuz/Gen0/twitter-account.png',
-      'https://ipfs.moralis.io:2053/ipfs/QmeAJDpYUzfjguoEqsvg7taZzoUryLohDLGCo5NREgQ2zX/Gen0/linkedin-account.png',
-      'https://ipfs.moralis.io:2053/ipfs/QmZ9wDPdjHAzC8WwqvbsqLPKkfiQNgwXT8hSkwKWrCMKY2/Gen0/github-account.png',
-    ]
+
+    const badgesData = {
+      '1-2': {
+        name: 'Verida Identity',
+        image:
+          'https://ipfs.moralis.io:2053/ipfs/QmbPHjLsp48QuSoCtDHvXRnfzAqRDYsX4udQXRQ5gB2w87/Gen0/verida-identity.png',
+      },
+      '2-2': {
+        name: 'Discord Account',
+        image:
+          'https://ipfs.moralis.io:2053/ipfs/QmQsghP7Y1dbZHgoo48hQz8SHdFk2mjmPikhkHg69HHhWb/Gen0/discord-account.png',
+      },
+      '1-3': {
+        name: 'Facebook Account',
+        image:
+          'https://ipfs.moralis.io:2053/ipfs/QmT1hqphvztijTtEKd1xdUMtPXFfKqrx7qRby3mpeLeiMQ/Gen0/facebook-account.png',
+      },
+      '2-3': {
+        name: 'Twitter Account',
+        image:
+          'https://ipfs.moralis.io:2053/ipfs/QmVZYkMfNC26LcwRJPdZmfUVGs5bmGFxz6AFpwwrAcVuuz/Gen0/twitter-account.png',
+      },
+      '1-4': {
+        name: 'LinkedIn Account',
+        image:
+          'https://ipfs.moralis.io:2053/ipfs/QmeAJDpYUzfjguoEqsvg7taZzoUryLohDLGCo5NREgQ2zX/Gen0/linkedin-account.png',
+      },
+      '2-4': {
+        name: 'Github Account',
+        image:
+          'https://ipfs.moralis.io:2053/ipfs/QmZ9wDPdjHAzC8WwqvbsqLPKkfiQNgwXT8hSkwKWrCMKY2/Gen0/github-account.png',
+      },
+      '1-5': {
+        name: 'Test Account',
+        // image:
+        //   'https://ipfs.moralis.io:2053/ipfs/QmeAJDpYUzfjguoEqsvg7taZzoUryLohDLGCo5NREgQ2zX/Gen0/linkedin-account.png',
+      },
+      '2-5': {
+        name: 'Test Account 2',
+        // image:
+        //   'https://ipfs.moralis.io:2053/ipfs/QmZ9wDPdjHAzC8WwqvbsqLPKkfiQNgwXT8hSkwKWrCMKY2/Gen0/github-account.png',
+      },
+      '1-6': {
+        name: 'Test Account 3',
+        // image:
+        //   'https://ipfs.moralis.io:2053/ipfs/QmeAJDpYUzfjguoEqsvg7taZzoUryLohDLGCo5NREgQ2zX/Gen0/linkedin-account.png',
+      },
+      '2-6': {
+        name: 'Test Account 4',
+        // image:
+        //   'https://ipfs.moralis.io:2053/ipfs/QmZ9wDPdjHAzC8WwqvbsqLPKkfiQNgwXT8hSkwKWrCMKY2/Gen0/github-account.png',
+      },
+    }
 
     for (let i = 0; i < numberOfCols; i++) {
       console.log('Enter col:', i)
-      rowOffset = -size * 3 + size / 2 + 20
+      rowOffset = -size * 3 + size / 2 + 16
       const extraRowOffset = (i * size) / 2 // - size / 4
       for (let j = 0; j < numberOfRows; j++) {
         console.log('Enter row:', j)
@@ -144,17 +177,24 @@ const Badges = () => {
           rowOffset + hexagonSize + extraRowOffset,
         ])
         rowOffset += size
-        if (imgIndex < images.length && i >= 1 && j >= 2) {
+        const badgePosition = `${i}-${j}` as any
+        if (badgesData[badgePosition]) {
+          const innePath = makeHexagonPath(hexagonSize + 6, [
+            colOffset + hexagonSize,
+            rowOffset - size + hexagonSize + extraRowOffset,
+          ])
+
           hexagons.push(
             <ClipImage
               key={'hex' + `${i}-${j}`}
-              imageSrc={images[imgIndex]}
+              imageSrc={badgesData[badgePosition].image}
               path={path}
-              offsetX={size / 5 - 6}
+              innerPath={innePath}
+              color={'#73b4be'}
+              offsetX={(size + 1) * (i - 1) + (size / 5 - 7.5) * (2 - i)}
               offsetY={rowOffset + extraRowOffset - size - 12}
             />
           )
-          imgIndex++
           continue
         }
 
@@ -162,7 +202,7 @@ const Badges = () => {
           <Path
             key={'hex' + `${i}-${j}`}
             path={path}
-            color={theme.color.primary}
+            color={'#73b4be'}
             style='stroke'
             strokeJoin='round'
             strokeWidth={0.5}>
@@ -176,34 +216,63 @@ const Badges = () => {
     return hexagons
   }
 
+  const checkIfWithinShape = (locationX, locationY, shapeProps) => {
+    if (shapeProps.type === 'circle') {
+      const { cx, cy, r } = shapeProps
+      const distanceSquared = (locationX - cx) ** 2 + (locationY - cy) ** 2
+      return distanceSquared <= r ** 2
+    }
+
+    if (shapeProps.type === 'rect') {
+      const { x, y, width, height } = shapeProps
+      return (
+        locationX >= x &&
+        locationX <= x + width &&
+        locationY >= y &&
+        locationY <= y + height
+      )
+    }
+
+    // Add support for other shapes if needed
+    return false
+  }
+
+  // TODO: Fix touch event
+  // Shape properties
+  const mainCircle = { type: 'circle', cx: 0, cy: 0, r: 200 }
+  const handleTouchEnd = (e) => {
+    const { locationX, locationY } = e.nativeEvent
+    // Check if the touch coordinates fall within the bounds of the shapes
+    if (checkIfWithinShape(locationX, locationY, mainCircle)) {
+      navigation.navigate('ClaimableBadges')
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Line style={{ marginTop: theme.spacing.s }} />
 
       <ScrollView>
-        <Canvas style={{ flex: 1, minHeight: 8 * size }}>
-          {/* <ClipImage
-          imageSrc={
-            'https://ipfs.moralis.io:2053/ipfs/QmbPHjLsp48QuSoCtDHvXRnfzAqRDYsX4udQXRQ5gB2w87/Gen0/verida-identity.png'
-          }
-        /> */}
+        <Canvas
+          style={{ flex: 1, minHeight: 8 * size }}
+          onTouchEnd={handleTouchEnd}>
+          <Rect x={0} y={0} width={width} height={8 * size}>
+            <RadialGradient
+              c={vec(width, 0)}
+              r={1283}
+              colors={['rgba(55, 213, 199, 0.06)', 'rgba(174, 71, 255, 0.1)']}
+              positions={[0, 0.5]}
+            />
+          </Rect>
+
           {drawHexagonGrid()}
-          {/* <Group blendMode='multiply'>
-          <Circle cx={r} cy={r} r={r} color='cyan' />
-          <Circle cx={size - r} cy={r} r={r} color='magenta' />
-          <Circle cx={size / 2} cy={size - r} r={r} color='yellow' />
-        </Group>
-        <Path
-          path='M 128 0 L 168 80 L 256 93 L 192 155 L 207 244 L 128 202 L 49 244 L 64 155 L 0 93 L 88 80 L 128 0 Z'
-          color='lightblue'
-        /> */}
         </Canvas>
       </ScrollView>
-      <Button
+      {/* <Button
         style={{ margin: theme.spacing.m }}
         onPress={() => navigation.navigate('ClaimableBadges')}>
         Claim Badges
-      </Button>
+      </Button> */}
     </View>
   )
 }
