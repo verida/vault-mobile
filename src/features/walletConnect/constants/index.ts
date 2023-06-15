84f3d49407b4a2dfd0439d2ae50477862b3f98f0
cawfree <hello@cawfree.com>
2023-06-15 18:59:57 +0100

    chore: narrow

--- src/features/walletConnect/constants/index.ts
+++ src/features/walletConnect/constants/index.ts
@@ -3,7 +3,7 @@ const WALLETCONNECT_PROTOCOL = 'wc:'
 //// Ex: wc:9145e975-4af0-4a28-a569-19aab7a21dd8@1?bridge=https%3A%2F%2F6.bridge.walletconnect.org&key=40dbb09f0eac060885a0edaf7f1ab7efba207c9b339bc49f805d61b615ac28a7
 export const isWalletConnectV1Connection = (
   maybeWalletConnectV1Connection: unknown
-) => {
+): maybeWalletConnectV1Connection is string => {
   if (typeof maybeWalletConnectV1Connection !== 'string') return false
 
   return (
@@ -15,7 +15,7 @@ export const isWalletConnectV1Connection = (
 //// Ex: 'wc:c034ac9bf61c23d3e551663ed8bf973c260130c12f89f22a35a5d1032e3c47af@2?relay-protocol=iridium&symKey=05f034367d195bca2532385b620bd2b2a6c5c62101050bdfe9253e283fe50e12'
 export const isWalletConnectV2Connection = (
   maybeWalletConnectV2Connection: unknown
-) => {
+): maybeWalletConnectV2Connection is string => {
   if (typeof maybeWalletConnectV2Connection !== 'string') return false
 
   return (
@@ -26,6 +26,6 @@ export const isWalletConnectV2Connection = (
 
 export const isWalletConnectConnection = (
   maybeWalletConnectConnection: unknown
-) =>
+): maybeWalletConnectConnection is string =>
   isWalletConnectV1Connection(maybeWalletConnectConnection) ||
   isWalletConnectV2Connection(maybeWalletConnectConnection)
