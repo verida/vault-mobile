170e62c1ff93090a1153deddd265001c7c7a9b58
aurelticot <aurelticot.dev@gmail.com>
2023-06-28 08:54:15 +1000

    Rename BehindAuthEventsRegister to BehindAuthHandlers

--- src/components/BehindAuthHandlers.tsx
+++ src/components/BehindAuthHandlers.tsx
@@ -2,7 +2,7 @@ import { useEventHandlers, useWatchPublicProfileChanges } from 'hooks'
 import { FC } from 'react'
 
 // Main place to register and handle app events after the user has authenticated
-export const BehindAuthEventsRegister: FC = () => {
+export const BehindAuthHandlers: FC = () => {
   useEventHandlers()
   useWatchPublicProfileChanges()
 
--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -2,7 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { BehindAuthContextProviders } from 'contexts'
 import React from 'react'
 
-import { BehindAuthEventsRegister } from 'components/BehindAuthEventsRegister'
+import { BehindAuthHandlers } from 'components/BehindAuthHandlers'
 import DashboardNavigator from 'navigation/DashboardNavigator'
 import { MainStackParams } from 'navigation/types'
 import DeleteAccount from 'pages/Account/DeleteAccount'
@@ -67,7 +67,7 @@ export const MainNavigator: React.FunctionComponent = () => {
   return (
     <>
       {/* An empty component, just to register all of the main app events after the user has authenticated. */}
-      <BehindAuthEventsRegister />
+      <BehindAuthHandlers />
 
       {/* As the MainNavigator is only mounted after the user is authenticated, so are these context providers. */}
       <BehindAuthContextProviders>
