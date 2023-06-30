64e68997ecc50f3031e389f67be4208cb4a8edfc
cawfree <hello@cawfree.com>
2023-06-30 11:04:32 +0100

    chore: chain mux

---
+++ src/features/walletConnect/hooks/useWalletConnectSessionRequestCallback.Near.ts
@@ -0,0 +1,9 @@
+import { IWeb3Wallet } from '@walletconnect/web3wallet'
+import { Web3WalletTypes } from '@walletconnect/web3wallet/dist/types/types/client'
+
+export const useWalletConnectSessionRequestCallbackNear = (): ((
+  web3wallet: IWeb3Wallet,
+  event: Web3WalletTypes.EventArguments['session_request']
+) => void) => {
+  return () => undefined
+}
--- src/features/walletConnect/hooks/useWalletConnectSessionRequestCallback.tsx
+++ src/features/walletConnect/hooks/useWalletConnectSessionRequestCallback.tsx
@@ -1,7 +1,10 @@
-import { formatJsonRpcError, formatJsonRpcResult } from '@json-rpc-tools/utils'
 import { IWeb3Wallet } from '@walletconnect/web3wallet'
 import { Web3WalletTypes } from '@walletconnect/web3wallet/dist/types/types/client'
-import { rejectSessionRequest } from 'features/walletConnect'
+import {
+  rejectSessionRequest,
+  useWalletConnectSessionRequestCallbackEthereum,
+  useWalletConnectSessionRequestCallbackNear,
+} from 'features/walletConnect'
 import * as React from 'react'
 
 // Acts as a multiplexer for WalletConnect session requests. It determines which
@@ -9,20 +12,22 @@ import * as React from 'react'
 export const useWalletConnectSessionRequestCallback = (): ((
   web3wallet: IWeb3Wallet,
   event: Web3WalletTypes.EventArguments['session_request']
-) => void) =>
-  React.useCallback(
+) => void) => {
+  const ethereum = useWalletConnectSessionRequestCallbackEthereum()
+  const near = useWalletConnectSessionRequestCallbackNear()
+
+  return React.useCallback(
     async (
       web3wallet: IWeb3Wallet,
       request: Web3WalletTypes.EventArguments['session_request']
     ) => {
       const maybeChainId = request?.params?.chainId
 
-      // TODO: relate network to chain
-      if (maybeChainId === 'ethereum') {
-        return
-      } else if (maybeChainId === 'near') {
-        return
-      }
+      // TODO: @cawfree We don't know what these are yet.
+      if (maybeChainId === 'ethereum') return ethereum(web3wallet, request)
+
+      // TODO: @cawfree We don't know what these are yet.
+      if (maybeChainId === 'near') return near(web3wallet, request)
 
       return rejectSessionRequest({
         web3wallet,
@@ -30,5 +35,6 @@ export const useWalletConnectSessionRequestCallback = (): ((
         reason: `Encountered unexpected chainId, "${maybeChainId}".`,
       })
     },
-    []
+    [ethereum, near]
   )
+}
