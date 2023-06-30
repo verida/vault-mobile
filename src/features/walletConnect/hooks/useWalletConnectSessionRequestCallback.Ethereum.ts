ef4be2485981f6cb3985f5f106e3eaf71502580b
cawfree <hello@cawfree.com>
2023-06-30 12:20:57 +0100

    fix: stub

--- src/features/walletConnect/hooks/useWalletConnectSessionRequestCallback.Ethereum.ts
+++ src/features/walletConnect/hooks/useWalletConnectSessionRequestCallback.Ethereum.ts
@@ -5,4 +5,7 @@ import * as React from 'react'
 export const useWalletConnectSessionRequestCallbackEthereum = (): ((
   web3wallet: IWeb3Wallet,
   event: Web3WalletTypes.EventArguments['session_request']
-) => Promise<void>) => React.useCallback(async () => undefined, [])
+) => Promise<void>) =>
+  React.useCallback(async () => {
+    throw new Error('EVM is currently unsupported.')
+  }, [])
