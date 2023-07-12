f26a7741321401271a0e486adec3c3f220d4f82b
cawfree <hello@cawfree.com>
2023-07-12 15:25:15 +0100

    fix: friendlyName

--- src/features/caip/utils/getSupportedCaipProtocolFriendlyName.ts
+++ src/features/caip/utils/getSupportedCaipProtocolFriendlyName.ts
@@ -2,13 +2,11 @@ import { ChainMetadatas, ParsedCaipType } from '../@types'
 import { stringifyCaip } from '../utils/stringifyCaip'
 import { getMaybeChainName } from './getMaybeChainName'
 
-const UNSUPPORTED_CHAIN_FRIENDLY_NAME = 'Unsupported Network'
-
 export function getSupportedCaipProtocolFriendlyName(
   chainMetadatas: ChainMetadatas,
   parsedCaipType: ParsedCaipType | null | undefined
 ): string {
-  if (!parsedCaipType) return UNSUPPORTED_CHAIN_FRIENDLY_NAME
+  if (!parsedCaipType) return 'Unknown'
 
   const caip = stringifyCaip({
     parsedCaipType,
