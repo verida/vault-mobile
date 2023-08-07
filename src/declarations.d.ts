38cf681a797fbbad8749e520c97951ed48536757
aurelticot <aurelticot.dev@gmail.com>
2023-08-07 18:27:07 +1000

    Add LOG_LEVEL to Env declaration

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,17 +1,18 @@
 declare module '*.svg' {
-  import React from 'react';
-  import { SvgProps } from 'react-native-svg';
+  import React from 'react'
+  import { SvgProps } from 'react-native-svg'
   const content: React.FC<
     SvgProps & {
-      fill?: string;
-      fillSecondary?: string;
+      fill?: string
+      fillSecondary?: string
     }
-  >;
-  export default content;
+  >
+  export default content
 }
 
-declare module "react-native-config" {
+declare module 'react-native-config' {
   interface Env {
+    LOG_LEVEL?: string
     INFURA_API_KEY: string
     SENTRY_ENVIRONMENT: string
     BITRISE_TRIGGERED_WORKFLOW_TITLE: string
