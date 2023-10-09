149be155040b06c4549348daaab72a21f66d6c7a
cawfree <hello@cawfree.com>
2023-10-09 15:00:11 +0100

    fix: unused import

--- src/pages/Tokens/SingleCurrency.tsx
+++ src/pages/Tokens/SingleCurrency.tsx
@@ -12,7 +12,6 @@ import {
   useGetTransactionsForTokenQuery,
   useMaybeBalanceForChainId,
   useMaybeBlockchainNetwork,
-  useWalletBannerBalance,
 } from 'features/cryptoWallet'
 import { Icon } from 'native-base'
 import React from 'react'
