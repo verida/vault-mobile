a5bbfa1fba217049369d096d5535b586b9712280
cawfree <hello@cawfree.com>
2023-11-12 14:21:34 +0000

    chore: add remove networks

--- src/features/blockchain/eip155/utils/chainsList.ts
+++ src/features/blockchain/eip155/utils/chainsList.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { ChainId } from 'caip'
-import { SupportedBlockchainNamespace } from 'features/blockchain/@types/enums'
 
 import { ChainMetadatas } from '../../../caip/@types'
+import { SupportedBlockchainNamespace } from '../../@types/enums'
 import {
   AddEthereumChainRequestParam,
   AddEthereumChainRequestParamBlockExplorerUrls,
--- src/features/caip/slice/actions.ts
+++ src/features/caip/slice/actions.ts
@@ -1,3 +1,4 @@
+import { IDatastore } from '@verida/types'
 import { ChainId } from 'caip'
 import { ethers } from 'ethers'
 
@@ -16,45 +17,130 @@ type AddCustomNetworkParams = {
   readonly reset?: boolean
 }
 
-export const addCustomNetwork = createAppAsyncThunk(
-  `${CAIP_SLICE_NAME}/addCustomNetwork`,
-  async (
-    { addCustomNetworkParams, reset = false }: AddCustomNetworkParams,
-    { rejectWithValue }
-  ) => {
-    try {
-      // TODO: @cawfree idk if this is correct
-      const vault = AccountManager.getInstance().context
+const chainMetadataToShoppingCoupon = (
+  chainMetadata: ChainMetadata
+): ShoppingCoupon => {
+  const { name: title } = chainMetadata
 
-      if (!vault)
-        throw new Error('Unable to allocate vault for custom networks.')
+  const value = JSON.stringify(chainMetadata)
 
-      const datastore = await vault.openDatastore(
-        HACK_SHOPPING_COUPON_DATASTORE
-      )
+  // TODO: We need a dedicated data model for this kind of information.
+  // TODO: The data model would also need to save a caip identifier.
+  return {
+    title,
+    description: 'A custom chain created using the Vault App.',
+    value,
+    valueType: 'CustomChain',
+    currency: 'Crypto',
+    barcode: ethers.utils.keccak256(ethers.utils.toUtf8Bytes(value)),
+  }
+}
 
-      if (reset) await datastore.deleteAll()
+const shoppingCouponToMaybeChainMetadata = (
+  shoppingCoupon: unknown
+): ChainMetadata | null => {
+  if (!shoppingCoupon || typeof shoppingCoupon !== 'object') return null
 
-      // TODO: parallelize
-      for (const addCustomNetworkParam of addCustomNetworkParams) {
-        const { name: title } = addCustomNetworkParam
+  if (!('value' in shoppingCoupon)) return null
 
-        const value = JSON.stringify(addCustomNetworkParam)
+  const { value: maybeValue } = shoppingCoupon
 
-        // TODO: We need a dedicated data model for this kind of information.
-        // TODO: The data model would also need to save a caip identifier.
-        const chainToAddAsShoppingCoupon: ShoppingCoupon = {
-          title,
-          description: 'A custom chain created using the Vault App.',
-          value,
-          valueType: 'CustomChain',
-          currency: 'Crypto',
-          barcode: ethers.utils.keccak256(ethers.utils.toUtf8Bytes(value)),
-        }
+  if (typeof maybeValue !== 'string') return null
 
+  let value: unknown = null
+
+  try {
+    value = JSON.parse(maybeValue)
+  } catch {}
+
+  const maybeParsed = ChainMetadata.safeParse(value)
+
+  if (!maybeParsed.success) return null
+
+  return maybeParsed.data
+}
+
+const getCustomNetworkDatastore = (): Promise<IDatastore> => {
+  // TODO: @cawfree idk if this is correct
+  const vault = AccountManager.getInstance().context
+
+  if (!vault) throw new Error('Unable to allocate vault for custom networks.')
+
+  return vault.openDatastore(HACK_SHOPPING_COUPON_DATASTORE)
+}
+
+const loadAllCustomNetworksFromDatastore = async ({
+  datastore,
+}: {
+  readonly datastore: IDatastore
+}): Promise<ChainMetadata[]> => {
+  // Read *all* the custom networks configurations back.
+  const [...results] = await datastore.getMany({}, undefined)
+
+  // Parse into correctly-formatted chains.
+  return results.flatMap((result): ChainMetadata[] => {
+    const maybeChainMetadata = shoppingCouponToMaybeChainMetadata(result)
+
+    if (!maybeChainMetadata) return []
+
+    // NOTE: Each individual RPC endpoint can be modelled as a dedicated ChainMetadata.
+    //       This coincides with the property that a ChainMetadatas can contain duplicate
+    //       configurations for the same network - ideally, it would be up to the user to
+    //       decide which RPC makes sense for which context; i.e. avoiding frontrunning
+    //       when transacting via Flashbots Protect, or using a home node.
+    return [maybeChainMetadata]
+  })
+}
+
+const batchModifyCustomNetworks = async ({
+  reset = false,
+  networksToAdd,
+  chainIdsToRemove,
+}: {
+  readonly networksToAdd: readonly ChainMetadata[]
+  readonly chainIdsToRemove: readonly ChainId[]
+  readonly reset?: boolean
+}) => {
+  const datastore = await getCustomNetworkDatastore()
+
+  // Conditionally reset the datastore, useful for development.
+  if (reset) await datastore.deleteAll()
+
+  // Get the existing networks.
+  const existingNetworks = await loadAllCustomNetworksFromDatastore({
+    datastore,
+  })
+
+  // Compute the next networks. Notice the priority: networks
+  // to add take higher order of precedence when it comes to
+  // de-duping.
+  const nextNetworks = [...networksToAdd, ...existingNetworks]
+
+  const nextNetworkChainIds = nextNetworks.map((e) => new ChainId(e).toString())
+
+  const nextNextworksWithoutDuplicates = nextNetworks.filter(
+    (_, i) => nextNetworkChainIds.indexOf(nextNetworkChainIds[i]) === i
+  )
+
+  const nextChainIdsToRemove = chainIdsToRemove.map((e) =>
+    new ChainId(e).toString()
+  )
+
+  // Finally, remove any networks which have identifiers which match chainIdsToRemove.
+  const nextNetworksWithoutChainsToRemove =
+    nextNextworksWithoutDuplicates.filter(
+      (e) => !nextChainIdsToRemove.includes(new ChainId(e).toString())
+    )
+
+  // Finally, let's save the new networks.
+  await datastore.deleteAll()
+
+  await Promise.all(
+    nextNetworksWithoutChainsToRemove.map(
+      async (chainMetadata: ChainMetadata) => {
         // TODO: devex - (any, any) - what do these mean?
         const result = await datastore.save(
-          chainToAddAsShoppingCoupon,
+          chainMetadataToShoppingCoupon(chainMetadata),
           undefined
         )
 
@@ -65,36 +151,26 @@ export const addCustomNetwork = createAppAsyncThunk(
         )
           throw new Error('Failed to save custom network!')
       }
+    )
+  )
 
-      // Read *all* the custom networks configurations back.
-      const [...results] = await datastore.getMany({}, undefined)
+  // Finally, read all of the chains back for persistence.
+  return loadAllCustomNetworksFromDatastore({
+    datastore,
+  })
+}
 
-      // Parse into correctly-formatted chains.
-      return results.flatMap((result): ChainMetadata[] => {
-        if (!result || typeof result !== 'object') return []
-
-        if (!('value' in result)) return []
-
-        const { value: maybeValue } = result
-
-        if (typeof maybeValue !== 'string') return []
-
-        let value: unknown = null
-
-        try {
-          value = JSON.parse(maybeValue)
-        } catch {}
-
-        const maybeParsed = ChainMetadata.safeParse(value)
-
-        if (!maybeParsed.success) return []
-
-        // NOTE: Each individual RPC endpoint can be modelled as a dedicated ChainMetadata.
-        //       This coincides with the property that a ChainMetadatas can contain duplicate
-        //       configurations for the same network - ideally, it would be up to the user to
-        //       decide which RPC makes sense for which context; i.e. avoiding frontrunning
-        //       when transacting via Flashbots Protect, or using a home node.
-        return [maybeParsed.data]
+export const addCustomNetwork = createAppAsyncThunk(
+  `${CAIP_SLICE_NAME}/addCustomNetwork`,
+  async (
+    { addCustomNetworkParams, reset = false }: AddCustomNetworkParams,
+    { rejectWithValue }
+  ) => {
+    try {
+      return batchModifyCustomNetworks({
+        networksToAdd: addCustomNetworkParams,
+        chainIdsToRemove: [],
+        reset,
       })
     } catch (error) {
       return rejectWithValue(`Failed to add custom network. ${String(error)}`)
@@ -108,29 +184,12 @@ type RemoveCustomNetworkParams = {
 
 export const removeCustomNetwork = createAppAsyncThunk(
   `${CAIP_SLICE_NAME}/removeCustomNetwork`,
-  async (
-    { chainIds }: RemoveCustomNetworkParams,
-    { rejectWithValue, getState }
-  ) => {
+  async ({ chainIds }: RemoveCustomNetworkParams, { rejectWithValue }) => {
     try {
-      const state = getState()
-
-      const {
-        customNetworks: { result },
-      } = state.caip
-
-      const chainIdsToDelete = chainIds.map((e) => e.toString())
-
-      const chainsThatSurvivedDeletion = result.filter(
-        ({ namespace, reference }) =>
-          !chainIdsToDelete.includes(
-            new ChainId({ namespace, reference }).toString()
-          )
-      )
-
-      // TODO: edit the state here
-
-      return chainsThatSurvivedDeletion
+      return batchModifyCustomNetworks({
+        networksToAdd: [],
+        chainIdsToRemove: chainIds,
+      })
     } catch (error) {
       return rejectWithValue(
         `Failed to remove custom network. ${String(error)}`
