43853896bddd2d3457a553946896b308e30077a6
cawfree <hello@cawfree.com>
2023-11-21 00:45:44 +0000

    chore: move away from serialization via ShoppingCoupon

--- src/features/caip/slice/actions.ts
+++ src/features/caip/slice/actions.ts
@@ -1,63 +1,23 @@
 import { IDatastore } from '@verida/types'
 import { ChainId } from 'caip'
-import { ethers } from 'ethers'
+import {
+  chainMetadataToMaybeCustomBlockchainNetwork,
+  CustomBlockchainNetwork,
+  customBlockchainNetworkToMaybeChainMetadata,
+} from 'features/blockchain'
 
 import AccountManager from 'api/AccountManager'
-import { ShoppingCoupon } from 'api/types'
 import { createAppAsyncThunk } from 'reduxStore/types'
 
 import { CAIP_SLICE_NAME, ChainMetadata } from '../@types'
 
 // HACK: For now, we'll save networks as shopping coupons. 🤡
-const HACK_SHOPPING_COUPON_DATASTORE =
-  'https://common.schemas.verida.io/shopping/coupon/v0.1.0/schema.json'
+const CUSTOM_BLOCKCHAIN_DATASTORE =
+  'https://vault.schemas.verida.io/blockchain/custom-networks/v0.1.0/schema.json'
 
 type AddCustomNetworkParams = {
   readonly addCustomNetworkParams: readonly ChainMetadata[]
   readonly reset?: boolean
-}
-
-const chainMetadataToShoppingCoupon = (
-  chainMetadata: ChainMetadata
-): ShoppingCoupon => {
-  const { name: title } = chainMetadata
-
-  const value = JSON.stringify(chainMetadata)
-
-  // TODO: We need a dedicated data model for this kind of information.
-  // TODO: The data model would also need to save a caip identifier.
-  return {
-    title,
-    description: 'A custom chain created using the Vault App.',
-    value,
-    valueType: 'CustomChain',
-    currency: 'Crypto',
-    barcode: ethers.utils.keccak256(ethers.utils.toUtf8Bytes(value)),
-  }
-}
-
-const shoppingCouponToMaybeChainMetadata = (
-  shoppingCoupon: unknown
-): ChainMetadata | null => {
-  if (!shoppingCoupon || typeof shoppingCoupon !== 'object') return null
-
-  if (!('value' in shoppingCoupon)) return null
-
-  const { value: maybeValue } = shoppingCoupon
-
-  if (typeof maybeValue !== 'string') return null
-
-  let value: unknown = null
-
-  try {
-    value = JSON.parse(maybeValue)
-  } catch {}
-
-  const maybeParsed = ChainMetadata.safeParse(value)
-
-  if (!maybeParsed.success) return null
-
-  return maybeParsed.data
 }
 
 const getCustomNetworkDatastore = (): Promise<IDatastore> => {
@@ -66,7 +26,7 @@ const getCustomNetworkDatastore = (): Promise<IDatastore> => {
 
   if (!vault) throw new Error('Unable to allocate vault for custom networks.')
 
-  return vault.openDatastore(HACK_SHOPPING_COUPON_DATASTORE)
+  return vault.openDatastore(CUSTOM_BLOCKCHAIN_DATASTORE)
 }
 
 const loadAllCustomNetworksFromDatastore = async ({
@@ -75,11 +35,16 @@ const loadAllCustomNetworksFromDatastore = async ({
   readonly datastore: IDatastore
 }): Promise<ChainMetadata[]> => {
   // Read *all* the custom networks configurations back.
-  const [...results] = await datastore.getMany({}, undefined)
+  const [...results] = await datastore.getMany<CustomBlockchainNetwork>(
+    {},
+    undefined
+  )
 
   // Parse into correctly-formatted chains.
   return results.flatMap((result): ChainMetadata[] => {
-    const maybeChainMetadata = shoppingCouponToMaybeChainMetadata(result)
+    const maybeChainMetadata = customBlockchainNetworkToMaybeChainMetadata({
+      customBlockchainNetwork: result,
+    })
 
     if (!maybeChainMetadata) return []
 
@@ -138,9 +103,17 @@ const batchModifyCustomNetworks = async ({
   await Promise.all(
     nextNetworksWithoutChainsToRemove.map(
       async (chainMetadata: ChainMetadata) => {
+        const maybeCustomBlockchainNetwork =
+          chainMetadataToMaybeCustomBlockchainNetwork({ chainMetadata })
+
+        if (!maybeCustomBlockchainNetwork)
+          throw new Error(
+            `Was unable to convert ChainMetadata to CustomBlockchainNetwork!`
+          )
+
         // TODO: devex - (any, any) - what do these mean?
         const result = await datastore.save(
-          chainMetadataToShoppingCoupon(chainMetadata),
+          maybeCustomBlockchainNetwork,
           undefined
         )
 
