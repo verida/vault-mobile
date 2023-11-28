b6ce1c63ef01671bab3b68c216734ded2799819c
Andy Nguyen <andy@verida.io>
2023-11-28 21:31:28 +0700

    feat: upgrade versions, fix build iOS and Android

--- android/app/src/main/java/io/verida/vault/MainActivity.java
+++ android/app/src/main/java/io/verida/vault/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 import expo.modules.splashscreen.singletons.SplashScreen;
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
@@ -28,13 +30,19 @@ public class MainActivity extends ReactActivity {
         return "main";
     }
 
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+     * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+     * (aka React 18) with two boolean flags.
+     */
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegateWrapper(this,
-                new ReactActivityDelegate(this, getMainComponentName())
-        );
+        return new DefaultReactActivityDelegate(
+                this,
+                getMainComponentName(),
+                // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+                DefaultNewArchitectureEntryPoint.getFabricEnabled());
     }
-
     /**
      * Align the back button behavior with Android S
      * where moving root activities to background instead of finishing activities.
