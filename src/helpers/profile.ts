19395d62904a9d69810c26f64c9dfb74734882bc
aurelticot <aurelticot.dev@gmail.com>
2023-12-05 18:23:03 +1100

    chore: remove unused components

--- src/helpers/profile.ts
+++
@@ -1,9 +0,0 @@
-const edit = (navigation: any, option: any) =>
-  navigation.navigate('EditProfile', { title: option.label, option })
-
-export const editable = (list: any) =>
-  list.map((option: any) => ({
-    ...option,
-    onPress: (navigation: any) =>
-      option.onPress ? option.onPress(option) : edit(navigation, option),
-  }))
