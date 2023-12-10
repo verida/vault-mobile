2e5db31faf680ae2ed7901d790c315487c77f64a
aurelticot <aurelticot.dev@gmail.com>
2023-12-10 17:13:59 +1100

    refactore: move BottomActionBar into ScreenLayouts folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 export * from './Alert'
 export * from './Images'
-export * from './Layouts'
 export * from './Status'
 export * from './Spacer'
+export * from './ScreenLayouts'
 export * from './Requests'
 export * from './Wallet'
 // TODO: Re-export all components from here
