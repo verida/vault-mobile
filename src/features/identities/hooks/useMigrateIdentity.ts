42aa33c178a079233c7dfa1866fe946cd5d6ce97
aurelticot <aurelticot.dev@gmail.com>
2023-12-22 00:15:07 +1100

    chore: fix typo, add comments and logs

--- src/features/identities/hooks/useMigrateIdentity.ts
+++ src/features/identities/hooks/useMigrateIdentity.ts
@@ -162,23 +162,21 @@ export function useMigrateIdentity() {
 
         // Get storage links (aka contexts) from the current DID document
         const links = await StorageLink.getLinks(
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
           currentClient.didClient,
           currentIdentity.did
         )
         logger.debug('links', { links })
 
         // Get the hashes of the contexts
-        const contextHashes: string[] =
-          IDENTITY_MIGRATION_USE_PREDEFINED_CONTEXTS
-            ? IDENTITY_MIGRATION_PREDEFINED_CONTEXT_NAMES
-            : links.map((link) => link.id)
+        // HACK: For now, we use a predefined list of context names as there is a bug when using the context hash
+        const contextHashes = IDENTITY_MIGRATION_USE_PREDEFINED_CONTEXTS
+          ? IDENTITY_MIGRATION_PREDEFINED_CONTEXT_NAMES
+          : links.map((link) => link.id)
 
         const nbContextsToMigrate = contextHashes.length
         logger.debug(`Number of contexts to migrate ${nbContextsToMigrate}`)
 
-        // TODO: Try optimise by running them in parallel, test if supported by the PouchDB replication
+        // TODO: Try optimise by running them in parallel, test if supported by the migration. Sequential is actual easier for the progress.
         for (let i = 0; i < contextHashes.length; i++) {
           const contextHash = contextHashes[i]
           try {
@@ -195,7 +193,7 @@ export function useMigrateIdentity() {
             if (!targetContext) {
               throw new Error('Could not open target context')
             }
-            logger.debug('Source target opened', { contextHash })
+            logger.debug('Target context opened', { contextHash })
 
             logger.debug('Migrating context', { contextHash })
             await migrateContext(sourceContext, targetContext, (progress) => {
