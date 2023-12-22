67c76d7924cf83aeadad4506ea261c193f0c3880
aurelticot <aurelticot.dev@gmail.com>
2023-12-22 13:12:41 +1100

    feat: fine-tune Assets tab header

--- src/components/WalletSelectorNavigation/WalletNavigationHeader.tsx
+++ src/components/WalletSelectorNavigation/WalletNavigationHeader.tsx
@@ -1,12 +1,13 @@
+import { Icon } from 'components'
+import { useTheme } from 'contexts'
 import { getTruncatedWalletAddress } from 'features/cryptoWallet'
+import { useThemeAwareStyle } from 'hooks'
 import React from 'react'
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 
-import ChevronDownIcon from 'assets/chevron_down_icon.svg'
-import MultichainWalletIcon from 'assets/wallet_icon_32.svg'
+import { Avatar } from 'components/Images'
 import { WalletItem } from 'components/WalletList/types'
-import { BLACK_COLOR } from 'constants/color'
-import { NUNITO_SANS, NUNITO_SANS_BOLD } from 'constants/text'
+import { Theme } from 'styles/types'
 
 interface WalletNavigationHeaderProps {
   selectedWallet: WalletItem
@@ -18,27 +19,33 @@ const HIT_SLOP = { top: 15, right: 15, bottom: 15, left: 15 }
 const WalletNavigationHeader = (props: WalletNavigationHeaderProps) => {
   const { selectedWallet, openWalletModal } = props
 
+  const { theme } = useTheme()
+  const styles = useThemeAwareStyle(createStyles)
+
   return (
     <Pressable
       hitSlop={HIT_SLOP}
       style={styles.container}
       onPress={openWalletModal}>
-      <View>
-        {selectedWallet?.icon ? (
-          <Image source={{ uri: selectedWallet.icon }} style={styles.icon} />
-        ) : (
-          <MultichainWalletIcon />
-        )}
+      <View style={styles.logoContainer}>
+        <Avatar
+          source={selectedWallet.icon}
+          fallbackType='wallet'
+          style={styles.icon}
+          borderColor={theme.color.primary100}
+          fallbackColor={theme.color.primary}
+          fallbackBackgroundColor={theme.color.primary200}
+        />
       </View>
-      <View style={styles.navigationContent}>
+      <View style={styles.textContainer}>
         <View style={styles.textWrapper}>
-          <Text style={styles.textTitle}>{selectedWallet?.label}</Text>
-          <View>
-            <ChevronDownIcon />
-          </View>
+          <Text style={styles.label} numberOfLines={1} ellipsizeMode='tail'>
+            {selectedWallet?.label}
+          </Text>
+          <Icon name='chevron-down' size={16} />
         </View>
         {selectedWallet ? (
-          <Text style={styles.subText}>
+          <Text style={styles.address} numberOfLines={1} ellipsizeMode='middle'>
             {selectedWallet.address
               ? getTruncatedWalletAddress(selectedWallet.address)
               : `${selectedWallet.count} addresses`}
@@ -51,31 +58,36 @@ const WalletNavigationHeader = (props: WalletNavigationHeaderProps) => {
 
 export default WalletNavigationHeader
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  textWrapper: {
-    flexDirection: 'row',
-  },
-  navigationContent: {
-    marginHorizontal: 10,
-  },
-  textTitle: {
-    fontSize: 14,
-    fontFamily: NUNITO_SANS_BOLD,
-    fontWeight: '600',
-    color: BLACK_COLOR,
-  },
-  subText: {
-    fontSize: 12,
-    fontFamily: NUNITO_SANS,
-    fontWeight: '600',
-    color: BLACK_COLOR,
-  },
-  icon: {
-    width: 32,
-    height: 32,
-  },
-})
+const createStyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    logoContainer: {
+      marginRight: theme.spacing.s,
+    },
+    textContainer: {
+      justifyContent: 'space-between',
+    },
+    textWrapper: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    label: {
+      fontSize: theme.fontSize.sl,
+      lineHeight: theme.fontSize.xl,
+      fontFamily: theme.fontFamily.bold,
+    },
+    address: {
+      fontSize: theme.fontSize.s,
+      lineHeight: theme.fontSize.m,
+      fontFamily: theme.fontFamily.semibold,
+      color: theme.color.textLightGrey,
+    },
+    icon: {
+      width: 32,
+      height: 32,
+    },
+  })
