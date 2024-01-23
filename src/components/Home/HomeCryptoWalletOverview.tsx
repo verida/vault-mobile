c8d9528cfad48336f42a6f8a0f31ac0500931667
aurelticot <aurelticot.dev@gmail.com>
2024-01-23 17:45:39 +1100

    feat: update token dashboard and list

--- src/components/Home/HomeCryptoWalletOverview.tsx
+++ src/components/Home/HomeCryptoWalletOverview.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native'
-import { Icon } from 'components'
+import { Icon, Typography } from 'components'
 import { useTheme } from 'contexts'
 import {
   getUniqueWalletAddresses,
@@ -8,7 +8,7 @@ import {
 } from 'features/cryptoWallet'
 import { useThemeAwareStyle } from 'hooks'
 import React from 'react'
-import { Pressable, StyleSheet, Text, View, ViewProps } from 'react-native'
+import { Pressable, StyleSheet, View, ViewProps } from 'react-native'
 import { useSelector } from 'react-redux'
 
 import { NumericFiat } from 'components/Span'
@@ -43,10 +43,12 @@ export const HomeCryptoWalletOverview: React.FC<CryptoWalletOverviewProps> = (
             <Icon name='wallet' size={24} color={theme.color.primary} />
           </View>
           <View>
-            <Text style={styles.walletLabel}>{displayedLabel}</Text>
-            <Text style={styles.walletAmount}>
+            <Typography variant='label' style={styles.walletLabel}>
+              {displayedLabel}
+            </Typography>
+            <Typography variant='h4' style={styles.walletValue}>
               <NumericFiat value={total || 0} />
-            </Text>
+            </Typography>
           </View>
         </View>
         <Icon name='chevron-forward' size={24} color={theme.color.primary} />
@@ -75,15 +77,9 @@ const createStyles = (theme: Theme) =>
       marginRight: theme.spacing.sm,
     },
     walletLabel: {
-      fontFamily: theme.fontFamily.semibold,
       color: theme.color.primary300,
-      fontSize: theme.fontSize.s,
-      lineHeight: theme.fontSize.s * 1.5,
     },
-    walletAmount: {
-      fontFamily: theme.fontFamily.bold,
+    walletValue: {
       color: theme.color.primary,
-      fontSize: theme.fontSize.sl,
-      lineHeight: theme.fontSize.sl * 1.3,
     },
   })
---
+++ src/components/Wallet/CryptoWalletValueBanner.tsx
@@ -0,0 +1,49 @@
+import BigDecimal from 'bignumber.js'
+import { useThemeAwareStyle } from 'hooks'
+import React from 'react'
+import { StyleSheet, View, ViewProps } from 'react-native'
+
+import { NumericFiat } from 'components/Span'
+import { Typography } from 'components/Typography'
+import { Theme } from 'styles/types'
+
+export type CryptoWalletValueBannerProps = {
+  value: BigDecimal
+} & ViewProps
+
+export const CryptoWalletValueBanner: React.FunctionComponent<CryptoWalletValueBannerProps> =
+  (props) => {
+    const { value, ...viewProps } = props
+
+    const styles = useThemeAwareStyle(createStyles)
+
+    return (
+      <View {...viewProps}>
+        <View style={styles.container}>
+          <Typography variant='label' style={styles.label}>
+            Total Value
+          </Typography>
+          <Typography variant='h3' style={styles.value}>
+            <NumericFiat value={value ? value.toNumber() : 0} />
+          </Typography>
+        </View>
+      </View>
+    )
+  }
+
+const createStyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      paddingVertical: theme.spacing.m,
+      borderRadius: theme.roundness.l,
+      flexDirection: 'column',
+      alignItems: 'center',
+      backgroundColor: theme.color.primary200,
+    },
+    label: {
+      color: theme.color.primary300,
+    },
+    value: {
+      color: theme.color.primary,
+    },
+  })
