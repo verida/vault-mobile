472fa9f303da86100a20febeccea236bb87261a8
aurelticot <aurelticot.dev@gmail.com>
2024-04-09 18:31:54 +1000

    refactor: move caip constant to blockchain feature folder

--- src/features/blockchain/near/constants/index.ts
+++ src/features/blockchain/near/constants/index.ts
@@ -1,10 +1,13 @@
-import { ChainId } from 'caip'
-import {
-  ChainMetadatas,
-  getRpcUrlOrThrow,
-  NEAR_TESTNET_CAIP,
-} from 'features/caip'
+import { ChainId, ChainIdParams } from 'caip'
 import { connect, keyStores } from 'near-api-js'
+
+import { ChainMetadatas, getRpcUrlOrThrow } from '~/features/caip'
+
+// TODO: It is not ideal to work this way (knowing a specific reference), however the implementation of Near protocol demands we do this for when we generate NEAR metadata URLs.
+export const NEAR_TESTNET_CAIP: ChainIdParams = {
+  namespace: 'near',
+  reference: 'testnet',
+}
 
 export async function getNearNetworkConfig({
   chainMetadatas,
